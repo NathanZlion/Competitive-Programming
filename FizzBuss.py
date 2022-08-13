0afbe480275193b2ea459ffedf2c54ad31dca59b
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-08-13 12:41:52 +0300

    Update FizzBuss.py

--- FizzBuss.py
+++ FizzBuss.py
@@ -1,19 +1,21 @@
-def FizzBuzz(num):
-  for i in range(num+1):
-    buzzed = True
-    if i%3==0:
-      print('Fizz', end = "")
-      buzzed = False
-    if i%5==0:
-      print('Buzz', end = "")
-      buzzed = False
-    if buzzed:
-      print(i, end = "")
-    print()
+class Solution:
+  def FizzBuzz(self, num):
+    for i in range(num+1):
+      buzzed = True
+      if i%3==0:
+        print('Fizz', end = "")
+        buzzed = False
+      if i%5==0:
+        print('Buzz', end = "")
+        buzzed = False
+      if buzzed:
+        print(i, end = "")
+      print()
  
 if __name__ == '__main__':
     try:
         num = int(input("Enter an integer: "))
-        FizzBuzz(num)
+        fizz = Solution()
+        fizz.FizzBuzz(num)
     except ValueError:
         print("Invalid Input, Please Enter an Integer.")
