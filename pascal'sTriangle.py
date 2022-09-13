442b1811ecadf9b37411a83f7b191513aebffc94
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-09-13 13:46:48 +0300

    Update pascal'sTriangle.py

--- pascal'sTriangle.py
+++ pascal'sTriangle.py
@@ -1,11 +1,5 @@
 class Solution(object):
     def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        
-        # construct the 2d array
         TwoDarr =[]
         
         for i in range(numRows):
