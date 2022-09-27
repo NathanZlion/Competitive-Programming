66d3afaf74480582e717872af5f2cd7d1d8d09c1
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-09-27 17:51:52 +0300

    Create Sort Colors.py

---
+++ Sort Colors.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        oneStart = 0
+        oneLength = 0
+        for i in nums:
+            if i == 0:
+                oneStart += 1
+            if i == 1:
+                oneLength += 1
+        
+        twoStart = oneStart + oneLength
+        for i in range(oneStart):
+            nums[i] = 0
+        for j in range(oneStart, twoStart):
+            nums[j] = 1
+        
+        for k in range(twoStart,len(nums)):
+            nums[k] = 2
+                
