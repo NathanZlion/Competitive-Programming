e2ef85c93a0872abf9a76633bc387637f20c5d78
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-10-14 16:09:27 +0300

    Update 509. FibonacciNumber.py

--- 509. FibonacciNumber.py
+++ 509. FibonacciNumber.py
@@ -1,7 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
+        if n < 2:
+            return n
         return self.fib(n-1) + self.fib(n-2)
