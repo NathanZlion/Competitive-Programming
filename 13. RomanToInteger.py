b016104f20ac7eca473aae9f301e3f119610fde8
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-10-20 10:13:17 +0300

    Update 13. RomanToInteger.py
    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
    
    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
    -> I can be placed before V (5) and X (10) to make 4 and 9. 
    -> X can be placed before L (50) and C (100) to make 40 and 90. 
    -> C can be placed before D (500) and M (1000) to make 400 and 900.
    
    Given a roman numeral, convert it to an integer.

--- 13. RomanToInteger.py
+++ 13. RomanToInteger.py
@@ -1,12 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # hash table of roman integers to integers
         dp = {'I':1,'V':5,'X':10,'L': 50,'C':100,'D':500,'M':1000,}
         if len(s)==1:return dp[s]
-        prev = acc = dp[s[-1]]
+        prev = acc = dp.get(s[-1])
         index, res = len(s)-2, 0
         while index>=0:
-            currNumber = dp[s[index]]
+            currNumber = dp.get(s[index])
             if currNumber >= prev:
                 res += acc
                 acc = currNumber
