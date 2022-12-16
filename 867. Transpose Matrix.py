78a87b8c9b8476454cfc390358a805149dec6903
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-12-16 19:19:19 +0300

    Update 867. Transpose Matrix.py

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -4,9 +4,6 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
-        """
-        matrix = [[1,2,3], [4,5,6], [7,8,9]]
-        """
         res=[]
         for j in range(len(matrix[0])):
             acc=[]
