28729b4455abdd6781e6448e226d48671322a229
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-12-16 19:36:10 +0300

    Update 2428. Maximum Sum of an Hourglass.py
    A better and readable way to do it.

--- 2428. Maximum Sum of an Hourglass.py
+++ 2428. Maximum Sum of an Hourglass.py
@@ -5,10 +5,18 @@ class Solution(object):
         :rtype: int
         """
         maxSum = 0
+        dirs = [[0,0],[0,1],[0,2],[1,1],[2,0],[2,1],[2,2]]
         def hourGlassSum(x,y):
-            return grid[x][y] + grid[x][y+1] + grid[x][y+2] + grid[x+1][y+1] + grid[x+2][y] + grid[x+2][y+1] + grid[x+2][y+2]
+            acc = 0
+            for [dx, dy] in dirs:
+                newRow = dx +x
+                newCol = dy +y
+                acc += grid[newRow][newCol]
+            return acc
 
         for j in range(len(grid[0])-2):
             for i in range(len(grid)-2):
                 maxSum = max(maxSum, hourGlassSum(i,j))
         return maxSum
+
+
