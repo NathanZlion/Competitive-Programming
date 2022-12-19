afa2094c409cbbe9190617c22c213db563c6bbb3
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-12-19 19:46:34 +0300

    Create Swap Case.py

---
+++ Swap Case.py
@@ -0,0 +1,8 @@
+def swap_case(s):
+    res = ""
+    for character in s:
+        if ord(character)  > 90:
+            res += character.upper()
+        else:
+            res += character.lower()
+    return res
