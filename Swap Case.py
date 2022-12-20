244535400daa2950d414e9984148d19b02e0434f
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-12-20 09:06:52 +0300

    Delete Swap Case.py

--- Swap Case.py
+++
@@ -1,8 +0,0 @@
-def swap_case(s):
-    res = ""
-    for character in s:
-        if ord(character)  > 90:
-            res += character.upper()
-        else:
-            res += character.lower()
-    return res
