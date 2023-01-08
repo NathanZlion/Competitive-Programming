1fec965fb5bac55f5170b31307e606ca374f69f1
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-01-08 18:45:27 +0300

    Update 1769. Minimum Number of Operations to Move All Balls to Each Box.py

--- PYTHON TRACK/1769. Minimum Number of Operations to Move All Balls to Each Box.py
+++ PYTHON TRACK/1769. Minimum Number of Operations to Move All Balls to Each Box.py
@@ -6,10 +6,12 @@ class Solution(object):
         """
         answer = [0] * len(boxes)
         for i in range(len(boxes)):
+            ans_at_i=0
             for j in range(len(boxes)):
                 if i==j:
                     continue
                 elif boxes[j]=='1':
-                    answer[i] += abs(j-i)
-        
+                    ans_at_i += abs(j-i)
+            answer[i] = ans_at_i
+
         return answer
