8fb5642a53f46e8244ca7c9e4350311de55dd540
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-01-20 13:22:54 +0300

    Update D. Beautiful Grid.py

--- Contest/contest_3/D. Beautiful Grid.py
+++ Contest/contest_3/D. Beautiful Grid.py
@@ -1,57 +1,44 @@
 
-
 t = int(input())
 
+def minimum_change(indexes, arr):
+    zeros = 0
+    ones = 0
+    for index in indexes:
+        if arr[index[0]][index[1]] == 0:
+            zeros += 1
+        else:
+            ones += 1
+    return min(zeros, ones)
 
-def minimum_change(lst):
-    # print(lst)
-    res = 0
-    for i in range(len(lst[0])-1):
-        zeros = 0
-        ones = 0
-        for arr in lst:
-            if arr[i] == "0":
-                zeros += 1
-            else:
-                ones += 1
-        
-        res += min(zeros, ones)
-    # print("res _" + str(res))
-    return res
-
+def transformations(row, col, n):
+    return [[row,col],[col,n-row-1],[n-row-1,n-col-1],[n-col-1,row]]
 
 for _ in range(t):
     n = int(input())
-    arr = []
 
-    # construct an array of strings
+    # construct an array of integers
+    r = []
     for _ in range(n):
-        arr.append(input())
+        r.append(input())
+    
+    arr = []
+    for row in r:
+        newR = []
+        for char in row:
+            newR.append(int(char))
+        arr.append(newR)
     
     col_left = 0
     col_right = len(arr[0])-1
-    row_top = 0
-    row_bottom = len(arr)-1
 
     changes = 0
-    while col_left < col_right and row_top < row_bottom:
-        arr_top = arr[row_top][col_left:col_right+1]
-        arr_bottom = arr[row_bottom][col_left:col_right+1]
-
-        arr_left = []
-        for row in range(row_top, row_bottom+1):
-            arr_left.append(arr[row][col_left])
-
-        arr_right = []
-        for row in range(row_top, row_bottom+1):
-            arr_right.append(arr[row][col_right])
-        
-        changes += minimum_change([arr_left ,arr_right, list(arr_top), list(arr_bottom)])
+    for row in range(n//2):
+        for col in range(col_left, col_right):
+            changes+=minimum_change(transformations(row,col,n), arr)
 
         col_left += 1
-        row_top += 1
         col_right -= 1
-        row_bottom -= 1
-    
-    print(">>>" + str(changes))
 
+
+    print(changes)
