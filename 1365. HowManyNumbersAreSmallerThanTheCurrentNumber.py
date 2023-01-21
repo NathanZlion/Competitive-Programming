1042c4dcd1ea9de3da97df8919f3cd60d6de7534
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-01-21 06:41:06 +0300

    Update 1365. HowManyNumbersAreSmallerThanTheCurrentNumber.py

--- 1365. HowManyNumbersAreSmallerThanTheCurrentNumber.py
+++ 1365. HowManyNumbersAreSmallerThanTheCurrentNumber.py
@@ -1,10 +1,18 @@
 class Solution(object):
-    def smallestEqual(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def smallerNumbersThanCurrent(self, nums):
+        counter = [0 for i in range(500)]
+        
+        for num in nums:
+            counter[num] += 1
+        
+        nums_lessthan = [0 for i in range(500)]
+
+        sum_ = 0
+        for num, occurance in enumerate(counter):
+            nums_lessthan[num] = sum_
+            sum_ += occurance
+
         for i in range(len(nums)):
-            if nums[i] == (i % 10):
-                return i
-        return -1
+            nums[i] = nums_lessthan[nums[i]]
+        
+        return nums
