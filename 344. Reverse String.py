7386fad505543fb35860c2e43f0f48d649f15046
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-01-21 05:32:27 +0300

    Create 344. Reverse String.py

---
+++ 344. Reverse String.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def reverseString(self, s):
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        
