bdcaa41a5f251399ac04982850fca2ecb63ee8f5
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-01-23 22:12:12 +0300

    Update 179. LargestNumber.py

--- 179. LargestNumber.py
+++ 179. LargestNumber.py
@@ -1,20 +1,19 @@
 class Solution(object):
     def largestNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: str
-        """
-        def larger(x,y):
+
+        def isLarger(x,y):
             return int(str(x) + str(y)) < int(str(y) + str(x))
 
         def bubbleSort(nums):
             for i in range(len(nums)-1):
                 for j in range(i,len(nums)):
-                    if larger(nums[i], nums[j]):
+                    if isLarger(nums[i], nums[j]):
                         nums[i], nums[j] = nums[j], nums[i]
-        
+
         bubbleSort(nums)
         strNum = []
         for num in nums:
             strNum.append(str(num))
+
+
         return str(int("".join(strNum)))
