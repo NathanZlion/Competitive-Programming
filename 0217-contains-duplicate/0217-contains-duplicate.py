a9c6a7890690b0e3cdbdbafae383ba7c3b62d9bd
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-01-30 23:20:46 +0300

    Time: 454 ms (46.57%), Space: 28.4 MB (14.20%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,4 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
         return len(nums) != len(set(nums))
         
