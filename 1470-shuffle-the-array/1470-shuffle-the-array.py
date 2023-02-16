9bec185ecd7f616a837be06d397707132d6b3ffa
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-02-16 23:32:52 +0300

    Time: 34 ms (95.96%), Space: 13.6 MB (73.69%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        arr1 = nums[:n]
-        arr2 = nums[n:]
         res = []
         for i in range(n):
-            res.append(arr1.pop(0))
-            res.append(arr2.pop(0))
+            res.append(nums[i])
+            res.append(nums[i+n])
         
         return res
         
