7e8c69fd3d9ecdf42a3639941c2a321176189b59
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-02-26 22:23:51 +0300

    Hacker's rank

---
+++ Array Manipulation.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    res = [0 for _ in range(n+1)]
+    
+    for query in queries:
+        res[query[0]-1] += query[2]
+        res[query[1]] -= query[2]
+    
+    for index in range(1,n):
+        res[index]+=res[index-1]
+    
+    return max(res[:n])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
