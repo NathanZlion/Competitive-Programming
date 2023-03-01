77f9224fa683416f7125033fa4e13dc3ff73e765
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-01 10:37:41 +0300

    Time: 34 ms (85.36%), Space: 13.8 MB (74.06%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        
+        seen = set()
+        mono_stack = []
+        count = Counter(s)
+        
+        for char in s:
+            if not mono_stack:
+                mono_stack.append(char)
+                seen.add(char)
+
+            elif char in seen:
+                pass
+
+            else:
+                while mono_stack and mono_stack[-1] >= char and count[mono_stack[-1]] > 0:
+                    seen.remove(mono_stack.pop())
+
+                mono_stack.append(char)
+                seen.add(char)
+        
+            count[char] -= 1
+        return "".join(mono_stack)
+                
+                    
+                    
+        
