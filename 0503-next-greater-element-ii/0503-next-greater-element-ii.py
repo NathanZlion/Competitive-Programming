07ca0e1b471901e450c9106bbbfacbadb5de7f00
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-03 15:49:25 +0300

    Time: 247 ms (36.51%), Space: 17.3 MB (6.53%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -7,15 +7,16 @@ class Solution:
         next_greater_at_index = {}
 
         for index in range(2*n):
-            while stack and stack[-1][1] < nums[index%n]:
+            ind = index%n
+            while stack and stack[-1][1] < nums[ind]:
                 if not stack[-1][0] in next_greater_at_index:
-                    next_greater_at_index[stack.pop()[0]] = nums[index%n]
+                    next_greater_at_index[stack.pop()[0]] = nums[ind]
 
-            stack.append([index, nums[index%n]])
+            stack.append([index, nums[ind]])
 
 
-        next_greater_at_index = defaultdict(lambda:-1 ,next_greater_at_index )
+        next_greater_at_index = defaultdict(lambda:-1, next_greater_at_index )
         for index in range(n):
             nums[index] = next_greater_at_index[index]
-        
+
         return nums
