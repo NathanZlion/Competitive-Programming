0990e41237f07b4fbaa708f7821f18880cebd7cd
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-06 18:08:54 +0300

    Time: 38 ms (22.55%), Space: 13.8 MB (59.88%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -5,26 +5,22 @@ class Solution:
             if char == '[':
                 stack.append(index)
             elif char == ']':
-                return [stack.pop(), index]                
+                return [stack.pop(), index]
 
-        
+
     def decodeString(self, s: str) -> str:
         if not '[' in s:
-            return s if not s.isnumeric() else ""
+            return s if not s.isdigit() else ""
 
-        # find the most inner bracket to decode
-        # stack the brackets until popped
         start, end = self.findBracketIndices(s)
-
         num_start_index = start-1
 
-        while s[num_start_index].isnumeric() and num_start_index >= 0:
+        while s[num_start_index].isdigit() and num_start_index > -1:
             num_start_index -= 1
-        
-        num_start_index += 1
-        num = int(s[num_start_index: start])
 
-        ans = s[:num_start_index] + (num* s[start+1:end]) + s[end+1:]
-        
+        num_start_index += 1
+        multiplier = int(s[num_start_index: start])
+
+        ans = s[:num_start_index] + (multiplier* s[start+1:end]) + s[end+1:]
+
         return self.decodeString(ans)
-        
