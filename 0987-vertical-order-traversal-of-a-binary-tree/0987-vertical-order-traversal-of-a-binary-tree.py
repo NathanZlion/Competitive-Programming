49595691dda08f2c7419ee24c3a9e63904518737
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-08 19:11:18 +0300

    Time: 40 ms (39.42%), Space: 14.3 MB (22.57%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,38 +6,34 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = {}
-        
-        def helper(root, row, col):
+        nodeRowColMap = {}
+
+        def traverse(root, row, col):
             if not root:
                 return
-            
-            res[root] =  [row, col]
 
-            helper(root.left, row +1, col-1)
-            helper(root.right, row+1, col+1)
-        
-        helper(root, 0, 0)
+            nodeRowColMap[root] =  [row, col]
+            traverse(root.left, row +1, col-1)
+            traverse(root.right, row+1, col+1)
+
+        traverse(root, 0, 0)
 
 
         colMap = defaultdict(list)
 
-        for node in res.keys():
-            colMap[res[node][1]].append(node)
-        
-        for column in colMap.keys():
-            colMap[column].sort(key = lambda x: (res[x][0], x.val) )
-        
-        colMapkeys = sorted(colMap)
-        
-        ans = []
+        for node in nodeRowColMap.keys():
+            colMap[nodeRowColMap[node][1]].append(node)
 
-        for column in colMapkeys:
+        for column in colMap.keys():
+            colMap[column].sort(key = lambda x: (nodeRowColMap[x][0], x.val) )
+
+        verticalTraversal = []
+
+        for column in sorted(colMap):
             col = []
             for node in colMap[column]:
                 col.append(node.val)
-            
-            ans.append(col)
 
-        return ans
-        
+            verticalTraversal.append(col)
+
+        return verticalTraversal
