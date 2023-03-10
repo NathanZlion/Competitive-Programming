e867021baccf98dccf42aa8c4dc95db5feda0934
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-10 05:34:01 +0300

    Time: 221 ms (67.57%), Space: 40 MB (27.41%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -14,8 +14,7 @@ class Solution {
         }
         return fairness_;
     }
-    
-    
+
     public void backTrack(int index){
         int curr_fairness = fairness(children_cookies);
         
@@ -46,34 +45,3 @@ class Solution {
         return minimum_fairness;
     }
 }
-
-
-
-
-// class Solution:
-//         def backtrack(index):
-            
-//             curr_maximum = max(self.children_cookies)
-
-//             if curr_maximum >= self.minimum:
-//                 return                
-
-//             if index == len(cookies):
-//                 self.minimum = min(self.minimum, curr_maximum)
-//                 return
-                
-//             for i in range(k):
-//                 self.children_cookies[i] += cookies[index]
-//                 backtrack(index+1)
-//                 self.children_cookies[i] -= cookies[index]
-
-//     def distributeCookies(self, cookies: List[int], k: int) -> int:
-
-//         self.children_cookies = [0 for _ in range(k)]
-//         self.minimum = float('inf')
-
-
-
-//         backtrack(0)
-
-//         return self.minimum
