1bd9d492c6e3b07900d1082d10603629e07f451b
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-10 04:44:48 +0300

    Time: 36 ms (65.13%), Space: 13.8 MB (97.04%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -12,15 +12,20 @@ class Solution:
 
             leftInt = integ(left)
             rightInt = integ(right)
+            
 
             if integ(right)+1 == leftInt:
                 self.possible  = True
                 return True
 
+            if rightInt < leftInt:
+                return False
+
             for i in range(1,len(right)):
                 if leftInt == integ(right[:i]) + 1:
                     if backTrack(right[:i], right[i:]):
                         return True
+            return False
 
         for i in range(1,len(s)):
             backTrack(arr[:i], arr[i:])
