efb5e89fa83b4e9c97038aa91f772aaea4289108
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-10 03:42:19 +0300

    Time: 32 ms (65.22%), Space: 13.8 MB (95.03%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -2,13 +2,11 @@ class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         if rowIndex < 2:
             return [1 for _ in range(rowIndex+1)]
-        
-        above = self.getRow(rowIndex-1)
-        
+
+        above = self.getRow(rowIndex-1)        
         res = [1 for _ in range(rowIndex+1)]
 
         for index in range(rowIndex-1):
             res[index+1] = above[index]+above[index+1]
 
-        print(res)
         return res
