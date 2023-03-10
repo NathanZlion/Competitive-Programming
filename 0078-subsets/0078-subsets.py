9d9facb0a13232d6f63d5afe77aa781f1937608f
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-10 12:30:23 +0300

    Time: 27 ms (96.99%), Space: 14.2 MB (27.78%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,23 @@
+class Solution:
+    
+    def backTrace(self, arr, index):
+        
+        if index == len(self.nums):
+            self.allSubsets.append(arr)
+            return
+        
+        self.backTrace(arr, index+1)
+        newarr = [i for i in arr]
+        newarr.append(self.nums[index])
+        self.backTrace(newarr, index+1)
+        
+    
+    
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.nums = nums
+        self.allSubsets = []
+        self.backTrace([], 0)
+        
+        
+        return self.allSubsets
+        
