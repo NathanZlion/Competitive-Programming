374dbab48cc57d4321f093863379de4985c2e1bb
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-16 15:29:38 +0300

    Time: 62 ms (68.93%), Space: 13.9 MB (57.32%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,8 +2,8 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         combinations = []
 
-        def backTrack(lst: List[int], index:int):
-            if sum(lst) > target:
+        def backTrack(sum_: int, lst: List[int], index:int):
+            if sum_ > target:
                 return 
 
             if sum(lst) == target:
@@ -12,11 +12,12 @@ class Solution:
 
             for idx in range(index, len(candidates)):
                 lst.append(candidates[idx])
-                backTrack(lst, idx)
+                backTrack(sum_ + candidates[idx],lst, idx)
                 lst.pop()
+                
 
 
-        backTrack([], 0)
+        backTrack(0, [], 0)
 
 
         return combinations
