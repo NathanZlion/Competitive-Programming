ce94f7b8dda2734b71cdd9b0e8f802c64b114b43
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-16 13:43:13 +0300

    Time: 30 ms (81.50%), Space: 13.8 MB (65.98%) - LeetHub

---
+++ 0306-additive-number/0306-additive-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+
+        def backTrack(lst, index):
+
+            if len(lst)>2 and lst[-1] != (lst[-2]+lst[-3]):
+                return False
+
+            if index == len(num):
+                print(lst)
+                return True if len(lst) > 2 else False
+            
+            if num[index] == 0:
+                return False
+
+            for end in range(index+1, len(num)+1):
+                lst.append(int(num[index:end]))
+
+                if backTrack(lst, end):
+                    return True and (num[index:end][0] != '0' or len(num[index:end]) == 1)
+
+                lst.pop()
+
+            return False
+
+        return backTrack([], 0)
