5a1c27ccd5477d48c254d03bdc635cb06ace8858
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-16 18:34:40 +0300

    Time: 109 ms (19.74%), Space: 13.8 MB (70.17%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,77 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        '''
+        -> there has to be a queen in every row of the board.
+        -> enumerate every possible position, while not violating the rule.
+        '''
+
+        def isValid(row, col):
+
+            # check column
+            for r in range(n):
+                if r != row and currState[r][col] == 1:
+                    return False
+            
+            # check diagonal top left
+            
+            r,c = row-1, col-1
+            
+            while r > -1 and c > -1:
+                if currState[r][c] == 1:
+                    return False
+                r -= 1
+                c -= 1
+
+            
+            # check diagonal bottom right
+
+            r,c = row+1, col+1
+            
+            while r < n and c < n:
+                if currState[r][c] == 1:
+                    return False
+                r += 1
+                c += 1
+
+
+            # check diagonal top right
+            
+            r,c = row-1, col+1
+            
+            while r > -1 and c < n:
+                if currState[r][c] == 1:
+                    return False
+                r -= 1
+                c += 1
+
+            # check diagonal bottom left
+            
+            r,c = row+1, col-1
+            
+            while r < n and c  > -1:
+                if currState[r][c] == 1:
+                    return False
+                r += 1
+                c -= 1
+            return True
+            
+            
+        currState = [[0 for _ in range(n)] for _ in range(n)]
+        solution = []
+        self.totalCount = 0
+
+        def backTrack(row):
+            if row == n:
+                self.totalCount += 1
+
+            for col in range(n):
+                if isValid(row, col):
+                    currState[row][col] = 1
+                    backTrack(row+1)
+                    currState[row][col] = 0
+
+        backTrack(0)
+        
+        return self.totalCount
+        
+        
