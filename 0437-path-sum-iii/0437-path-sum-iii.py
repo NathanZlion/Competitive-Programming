f6aee3b5725d1e97f8b452920e67c93cb091d09f
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-17 17:03:24 +0300

    Time: 49 ms (88.35%), Space: 15.4 MB (53.13%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+
+        '''
+        backtracking branches:
+         -> start from that node and go down.
+         -> add that node to the sum and go down.
+        '''
+
+        self.sum_ = 0
+        dict = defaultdict(int)
+        dict[0] = 1
+        self.countOfPaths = 0
+
+        def backTrack(node: TreeNode):
+            if not node:
+                return
+            
+            self.sum_ += node.val
+
+            self.countOfPaths += dict[self.sum_ - targetSum]
+            
+            dict[self.sum_] += 1
+            backTrack(node.left)
+            backTrack(node.right)
+            dict[self.sum_] -= 1
+            self.sum_ -= node.val
+            
+        
+        backTrack(root)
+
+        return self.countOfPaths
