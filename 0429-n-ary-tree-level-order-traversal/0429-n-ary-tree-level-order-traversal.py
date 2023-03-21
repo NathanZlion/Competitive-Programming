8518f423d9e519631c1bf9d94778bd548fe9045b
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-21 17:24:31 +0300

    Time: 56 ms (51.78%), Space: 15.9 MB (91.73%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.py
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.py
@@ -10,17 +10,19 @@ class Solution:
     def levelOrder(self, root: 'Node') -> List[List[int]]:
         if root is None:
             return []
+
         queue = deque()
         queue.append(root)
         traversedOrder = []
+
         while queue:
             length = len(queue)
             value = []
             for _ in range(length):
                 root_val = queue.popleft()
                 for child in root_val.children:
-                    if child is not None:
-                        queue.append(child)
+                    queue.append(child)
+
                 value.append(root_val.val)
             traversedOrder.append(value)
         
