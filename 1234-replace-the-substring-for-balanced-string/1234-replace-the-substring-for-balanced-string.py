070926f56e3b2b87fde7ff6a02518c5442a97a11
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-21 16:05:31 +0300

    Time: 479 ms (13.13%), Space: 14.8 MB (29.97%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -0,0 +1,37 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        n = len(s)
+        minimumLengthSubstring = n
+        ctr = Counter(s)
+        left = 0
+        
+        for right in range(n):
+            ctr[s[right]] -= 1
+            while left <= right:
+                ctr[s[left]] += 1
+                left += 1
+                if not self.isBalanced(ctr, n//4):
+                    left -= 1
+                    ctr[s[left]] -= 1
+                    break
+            
+            if self.isBalanced(ctr, n//4):
+                minimumLengthSubstring = min(minimumLengthSubstring, right - left + 1)
+            
+        
+        return minimumLengthSubstring            
+            
+        
+        
+        
+        return -1
+    
+    def isBalanced(self, ctr, n):
+        for k in ctr.keys():
+            if ctr[k] > n:
+                return False
+        
+        return True
+        
+    
+        
