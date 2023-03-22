ba1f0c12ce132c9df996ffddbd9ed2fda048594b
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-22 22:21:02 +0300

    Time: 162 ms (75.92%), Space: 14.2 MB (16.05%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,60 +1,64 @@
 
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-
-        def check(row: int, col: int, digit: str) -> bool:
-            # check if there's no duplicate in that column
-            for c in range(9):
-                if c != col and board[row][c] == digit:
-                    return False
-                            
-            # check if there's no duplicate in that row
-            for r in range(9):
-                if r != row and board[r][col] == digit:
-                    return False
-            
-            # check if there's no duplicate in that specific 3X3 sub box
-            rowStart, colStart = row - (row%3), col - (col%3)
-
-            for r in range(rowStart, rowStart+3):
-                for c in range(colStart, colStart+3):
-                    if (r != row) and (c != col) and (board[r][c] == digit): 
-                        return False
-
-            return True
-
         
-        def solve(row, col) -> bool:
+        rows = defaultdict(lambda: set())
+        cols = defaultdict(lambda: set())
+        subboxes = defaultdict(lambda: set())
+        emptyBox = deque()
 
-            row += (col // 9)
-            col %= 9
+        # building the rows, cols, subboxes dictionary
+        # plus holidng the empty spots in emptybox
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == ".":
+                    emptyBox.appendleft((row,col))
+                else:
+                    rows[row].add(board[row][col])
+                    cols[col].add(board[row][col])
+                    subboxes[((3 * (row//3)), (col//3))].add(board[row][col])
 
-            # find the next empty space in the board            
-            while row < 9:
-                if board[row][col] == '.':
-                    break
-                col += 1
-                row += (col // 9)
-                col %= 9
+        # print(subboxes)
 
-            # finished solving
-            if  row > 8:
+        def solve():
+            if len(emptyBox) == 0:
                 return True
 
-            # try out valid placeholders in it and backTrack
-            for digit in range(1,10):
+            row, col = emptyBox.pop()
 
-                if check(row, col, str(digit)):
-                    board[row][col] = str(digit)
+            for num in map(str, range(1, 10)):
+                if not (num in rows[row] or num in cols[col] or num in subboxes[((3 * (row//3)), (col//3))]):
+                    board[row][col] = num
+                    rows[row].add(num)
+                    cols[col].add(num)
+                    subboxes[((3 * (row//3)), (col//3))].add(num)
 
-                    if solve(row, col+1):
+                    if solve():
                         return True
 
-                    board[row][col] = '.'
+                    board[row][col] = "."
+                    rows[row].remove(num)
+                    cols[col].remove(num)
+                    subboxes[((3 * (row//3)), (col//3))].remove(num)
 
+            emptyBox.append((row, col))
+            
             return False
 
-        solve(0, 0)
+        solve()
+
+        """
+        iterate once to hold the current dictionaries:
+            row, column, subbox, emptyPositions(deque)
+
+        backTrack:
+            basecases:
+                there are no emptyPositions: => solved, return true
+
+                for each possibility in next empty space:
+                    if its a valid number:
+                        if backTrack:
+                            return True
+
+                return false i.e dead end
+        """
