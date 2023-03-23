5fe6ab97e35f923718c65395c702f3e3971e829f
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-23 21:51:27 +0300

    Time: 830 ms (63.62%), Space: 50 MB (37.75%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -4,19 +4,13 @@ class Solution:
         # start at any node and build candidates
 
         graph = defaultdict(set)
-        all_nodes = set()
 
         # constructing the graph
         for edge in edges:
-            all_nodes.add(edge[0])
-            all_nodes.add(edge[1])
             graph[edge[0]].add(edge[1])
             graph[edge[1]].add(edge[0])
 
-        # for each node check if it has all the nodes except self...
-        nodes = all_nodes.copy()
-        for node in nodes:
-            all_nodes.remove(node)
-            if graph[node] == all_nodes:
-                return node
-            all_nodes.add(node)
+            if len(graph[edge[0]]) > 1:
+                return edge[0]
+            if len(graph[edge[1]]) > 1:
+                return edge[1]
