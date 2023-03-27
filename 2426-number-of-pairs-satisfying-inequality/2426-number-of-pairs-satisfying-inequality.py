5716266212258ecece6bab47e600bd3459876430
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-27 20:06:20 +0300

    Time: 2524 ms (14.97%), Space: 33.1 MB (32.39%) - LeetHub

---
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -0,0 +1,58 @@
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
+        n = len(nums1)
+        difference = [0 for _ in range(n)]
+        
+        for i in range(n):
+            difference[i] = nums1[i] - nums2[i]
+
+        self.pairCount = 0
+
+        def mergeSort(nums):
+
+            if len(nums) == 1:
+                return nums
+
+            leftHalf = mergeSort(nums[:len(nums)//2])
+            rightHalf = mergeSort(nums[len(nums)//2:])
+
+            ptr2 = 0
+
+            for ptr1 in range(len(leftHalf)):
+                while ptr2 < len(rightHalf) and rightHalf[ptr2]+diff < leftHalf[ptr1]:
+                    ptr2 += 1
+
+                self.pairCount += len(rightHalf) - ptr2
+
+            return merge(leftHalf, rightHalf)
+
+        
+        def merge(arr1, arr2):
+            res = []
+            
+            ptr1 = ptr2 = 0
+            while ptr1 < len(arr1) and ptr2 < len(arr2):
+                if arr1[ptr1] <= arr2[ptr2]:
+                    res.append(arr1[ptr1])
+                    ptr1 += 1
+                
+                else:
+                    res.append(arr2[ptr2])
+                    ptr2 += 1
+
+
+            while ptr1 < len(arr1):
+                res.append(arr1[ptr1])
+                ptr1 += 1
+
+            while ptr2 < len(arr2):
+                res.append(arr2[ptr2])
+                ptr2 += 1
+
+            return res
+    
+        mergeSort(difference)
+        return self.pairCount
+
+
+
