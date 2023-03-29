bb7eaea8f8aff6ae3e9e58fe47e9d3d1b4fd4188
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-29 18:44:42 +0300

    Time: 2 ms (57.23%), Space: 46.5 MB (68.81%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,24 +2,22 @@ class Solution {
     Map<Integer, Integer> memo;
 
     public int countOnes(int number){
-        if (memo.containsKey(number)) return memo.get(number);
-
-        if (number % 2 == 0) return countOnes(number/2);
-
-        return 1 + countOnes(number/2);
+        
+        int bitCount = 0;
+        while (number > 0){
+            bitCount += number & 1;
+            number >>= 1;
+        }
+        
+        return bitCount;
     }
 
     public int[] countBits(int n) {
-        
-        this.memo = new HashMap();
-        memo.put(0,0);
-        memo.put(1,1);
 
         int[] res = new int[n+1];
 
         for (int i = 0; i < n+1; i++){
-            memo.put(i, countOnes(i));
-            res[i] = memo.get(i);
+            res[i] = countOnes(i);
         }
 
         return res;
