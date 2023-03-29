56f7f02aa525e238c4b39618b361d6f5ee69ff0f
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-29 18:05:02 +0300

    Time: 172 ms (18.48%), Space: 20.7 MB (66.00%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,20 +1,16 @@
 class Solution:
-
-    def numberOfOnes(self, number: int) -> Optional[List[int]]:
-        onesCount = 0
-
-        while number:
-            onesCount += number%2
-            number//=2
-
-        return onesCount
-
+    def numberOfBits(self, num : int) -> int:
+        bitCount = 0
+        while num:
+            bitCount += num%2
+            num //= 2
+        
+        return bitCount
 
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        
-        for i in range(n+1):
-            ans.append(self.numberOfOnes(i))
+        arr = [0 for _ in range(n+1)]
 
-        return ans
-        
+        for i in range(n+1):
+            arr[i] = self.numberOfBits(i)
+
+        return arr
