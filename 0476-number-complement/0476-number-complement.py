1c11d6c4669e8b37b95822d1203508bf46f404a6
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-03-30 13:31:19 +0300

    Time: 31 ms (67.32%), Space: 13.9 MB (40.50%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,15 +1,15 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         binaries = []
-        
+
         while num:
             binaries.append(num%2)
             num //= 2
-        
+
         res = 0
-        while binaries:
+        for i in range(len(binaries)-1, -1, -1):
             res<<=1
-            res += 0 if binaries.pop() else 1
-        
-        
+            res += 0 if binaries[i] else 1
+
+
         return res
