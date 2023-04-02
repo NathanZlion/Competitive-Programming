8c689cd67cfd45f5019f5ff79d47b27f52528311
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-02 20:46:25 +0300

    Time: 181 ms (6.51%), Space: 18.9 MB (32.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,34 @@
 class Solution:
+
+    def quickSort(self, left, right, arr):
+        if right - left < 1:
+            return
+        
+        pivot = arr[left]
+        write = left + 1
+
+        for read in range(left+1, right+1):
+            if self.freq[arr[read]] <= self.freq[pivot]:
+                arr[write], arr[read] = arr[read], arr[write]
+                write += 1
+        arr[left], arr[write-1] = arr[write-1], arr[left]
+
+        self.quickSort(left, write-2, arr)
+        self.quickSort(write, right, arr)        
+
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = defaultdict(int)
+        self.freq = defaultdict(int)
         numsSet = set()
 
         for num in nums:
-            freq[num] += 1
+            self.freq[num] += 1
             numsSet.add(num)
-        
-        
-        res = []
+
+        arr = []
         for num in numsSet:
-            res.append(num)
+            arr.append(num)
 
-        res.sort(key = lambda x: freq[x])
+        self.quickSort(0, len(arr)-1, arr)
 
-        return res[len(res)-k:]
-
-        
+        return arr[len(arr)-k:]
