5b3a4dabe3783fde5c2df9e4655b4aff399d1bcb
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-07 19:11:22 +0300

    Time: 644 ms (79.98%), Space: 19.5 MB (84.69%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,62 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        '''
+        0: sea cell,
+        1: land cell,
+
+        number of land cells in gris which we can't walk off boundary of grid.
+        from all boundaries try discovering neighbors.
+
+        '''
+        
+        neighbors = [(-1,0),(0,-1),(1,0),(0,1)]
+        
+        rows, cols = len(grid), len(grid[0])
+        
+        stack = []
+
+        # top and bottom boundary
+
+        firstRow = firstCol = 0
+        lastRow , lastCol = rows-1, cols-1
+
+        for col in range(cols):
+            if grid[firstRow][col] == 1:
+                stack.append((firstRow, col))
+            
+            if grid[lastRow][col] == 1:
+                stack.append((lastRow, col))
+        
+        for row in range(rows):
+            if grid[row][firstCol] == 1:
+                stack.append((row, firstCol))
+            
+            if grid[row][lastCol] == 1:
+                stack.append((row, lastCol))
+        
+        while stack:
+            currRow, currCol = stack.pop()
+            
+            grid[currRow][currCol] = 0
+
+            for neighbor in neighbors:
+                neighborRow, neighborCol = neighbor
+                neighborRow += currRow
+                neighborCol += currCol
+
+                if neighborRow < 0 or neighborCol < 0 or neighborRow >= rows or neighborCol >= cols:
+                    continue
+                
+                if grid[neighborRow][neighborCol] == 1:
+                    stack.append((neighborRow, neighborCol))
+                
+        ctr = 0
+        for row in range(rows):
+            for col in range(cols):
+                ctr += grid[row][col]
+        
+        return ctr            
+        
+        
+        
+                
