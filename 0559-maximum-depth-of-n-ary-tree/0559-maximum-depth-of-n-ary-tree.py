1b8d2333b127b4e02be7ccaa85e0434b5e4c43ff
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-09 11:54:18 +0300

    Time: 53 ms (21.23%), Space: 16.1 MB (39.90%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -7,22 +7,28 @@ class Node:
 """
 
 class Solution:
-    
-    def depth(self, node: 'Node', currDepth: int) -> int:
-        if not node:
-            return currDepth
-
-        maxDepth = currDepth + 1
-
-        for neighbor in node.children:
-            maxDepth = max(self.depth(neighbor, currDepth +1), maxDepth)
-        
-        return maxDepth
-
-        
 
     def maxDepth(self, root: 'Node') -> int:
-        return self.depth(root, 0)
+        
+        if not root:return 0
+
+        # iterative bfs
+        queue = deque()
+        queue.append(root)
+        
+        currDepth = 0
+
+        while len(queue) > 0:
+            currDepth += 1
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                
+                for neighbor in curr.children:
+                    queue.append(neighbor)
+
+        return currDepth
+                
+                
+            
         
     
-        
