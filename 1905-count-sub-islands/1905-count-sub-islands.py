9e3e8075db94a230f84f647fcf0141c54bdb19cc
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-12 19:20:11 +0300

    Time: 3129 ms (56.99%), Space: 88.2 MB (45.61%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -3,7 +3,6 @@ class Solution:
         # find islands in grid 2
         # check if it's a sub island.
         
-        island = []
         rowLength = len(grid2)
         colLength = len(grid2[0])
         directions = [(0,1),(1,0),(0,-1),(-1,0)]
@@ -12,30 +11,23 @@ class Solution:
         def isInboundAndLand(row, col):
             return 0 <= row < rowLength and 0 <= col < colLength and grid2[row][col] == 1
 
-        def isSubIsland(island: List[int]) -> bool:
-            for row, col in island:
-                if grid1[row][col] == 0:
-                    return False
+        def dfs(row, col) -> bool:
             
-            return True
+            valid = grid1[row][col] == 1
+            
 
-
-        def dfs(row, col) -> None:
-            nonlocal island
-
-            island.append((row, col))
             grid2[row][col] = 0
 
             for r, c in directions:
                 if isInboundAndLand(row + r, col + c):
-                    dfs(row + r, col + c)
+                    valid = dfs(row + r, col + c) and valid
 
-        
+            return valid
+
+
         for row in range(rowLength):
             for col in range(colLength):
                 if isInboundAndLand(row, col):
-                    island.clear()
-                    dfs(row, col)
-                    count += 1 if isSubIsland(island) else 0
+                    count += 1 if dfs(row, col) else 0
 
         return count
