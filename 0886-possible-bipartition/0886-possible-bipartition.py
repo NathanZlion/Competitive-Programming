ae108f6fa942a43f72c1e2f6401a5dee27a7d6b3
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-14 20:54:09 +0300

    Time: 723 ms (48.56%), Space: 22.2 MB (16.16%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -0,0 +1,33 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        dislikeList = defaultdict(list)
+
+        for edge in dislikes:
+            dislikeList[edge[0]].append(edge[1])
+            dislikeList[edge[1]].append(edge[0])
+
+        color = [0 for _ in range(n+1)]
+
+        def dfs(index):
+            if color[index] == 0:
+                color[index] = -1
+            
+            for neighbor in dislikeList[index]:
+                if color[neighbor] == 0:
+                    color[neighbor] = color[index] * -1
+                    if not dfs(neighbor):
+                        return False
+
+                elif color[neighbor] == color[index]:
+                    return False
+            
+            return True
+
+
+        for index in range(2, n+1):
+            if color[index-1] == 0:
+                if not dfs(index):
+                    return False
+
+        return True
+
