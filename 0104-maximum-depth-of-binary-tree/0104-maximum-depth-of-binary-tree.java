8f37982076e465a151a328d3f57338f94e5f64a0
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-25 11:33:17 +0300

    Time: 0 ms (100.00%), Space: 42.4 MB (49.75%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,10 +15,8 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null){
-            return 0;
-        }
-        
+        if (root == null) return 0;
+
         return Math.max(maxDepth(root.right), maxDepth(root.left)) + 1;
     }
 }
