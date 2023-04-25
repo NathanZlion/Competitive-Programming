f56e8f1ad39b411ee1cc20852a40378f20ca807e
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-25 11:20:02 +0300

    Time: 48 ms (82.86%), Space: 18.6 MB (53.29%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,25 +6,25 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        # height of the left and right subtree of any node differ by not more than 1.
+        # height-balanced = height of the left and right subtree of any node differ by not more than 1.
 
         if not root:
             return True
-        
+
         isBalanced : bool = True
 
-        def dfs(node: TreeNode) -> int:
+        def dfs(node: Optional[TreeNode]) -> int:
             nonlocal isBalanced
 
-            if not node:
+            if not node or not isBalanced:
                 return 0
-            
-            leftDepth = dfs(node.left) if node.left else 0
-            rightDepth = dfs(node.right) if node.right else 0
-            
+
+            leftDepth = dfs(node.left)
+            rightDepth = dfs(node.right)
+
             if abs(rightDepth - leftDepth) > 1:
                 isBalanced = False
-            
+
             return max(leftDepth, rightDepth) + 1
 
         dfs(root)
