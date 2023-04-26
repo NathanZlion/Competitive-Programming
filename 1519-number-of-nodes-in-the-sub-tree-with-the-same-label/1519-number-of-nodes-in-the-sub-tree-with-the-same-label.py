4c38c09ee68b8754cced1ce6a87e096f19415a36
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-26 18:57:19 +0300

    Time: 2480 ms (41.00%), Space: 200.2 MB (27.00%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    def mergeLists(self, list1: List[int], list2: List[int]) -> None:
+        for index in range(26):
+            list1[index] += list2[index]
+
+
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+
+        # convert edges to adjecency list: undirected graph
+        adjList = defaultdict(list)
+        for a, b in edges:
+            adjList[a].append(b)
+            adjList[b].append(a)
+
+        res = [1 for _ in range(n)]
+        visited = set()
+
+        def dfs(node: int) -> List[int]:
+            nonlocal res
+
+            labelCount = [0 for _ in range(26)]
+
+            for child in adjList[node]:
+                if not child in visited:
+                    visited.add(child)
+                    self.mergeLists(labelCount, dfs(child))
+
+            nodeLabelIndex = ord(labels[node]) - 97
+            res[node] += labelCount[nodeLabelIndex]
+            labelCount[nodeLabelIndex] += 1
+
+            return labelCount
+        
+        for node in range(n):
+            if node not in visited:
+                visited.add(node)
+                dfs(node)
+
+        return res
+
