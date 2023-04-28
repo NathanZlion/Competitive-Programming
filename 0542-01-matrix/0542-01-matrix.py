d55fd27b144fe3605b122d510cdd122e5e468ec6
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-04-28 18:07:58 +0300

    Time: 732 ms (32.87%), Space: 19.1 MB (5.01%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,7 +1,6 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
 
-
         totalRows = len(mat)
         totalCols = len(mat[0])
     
@@ -11,21 +10,23 @@ class Solution:
             return 0 <= row < totalRows and 0 <= col < totalCols
 
         queue = deque()
+
         for row in range(totalRows):
             for col in range(totalCols):
+                
+                # add all 0 locations to queue as they are the sources
                 if mat[row][col] == 0:
                     queue.append((row, col))
                 else:
                     mat[row][col] = float('inf')
 
-
         while len(queue) > 0:
             row, col = queue.popleft()
             
             for r, c in directions:
-                if isInbound(row + r, col + c):
-                    if mat[row+r][col+c] > mat[row][col]+1:
-                        mat[row+r][col+c] = mat[row][col]+1
-                        queue.append((row+r, col+c))
-        
+                if isInbound(row + r, col + c) and mat[row+r][col+c] > mat[row][col]+1:
+                    mat[row+r][col+c] = mat[row][col]+1
+                    queue.append((row+r, col+c))
+
+
         return mat
