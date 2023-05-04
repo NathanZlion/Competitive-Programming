9a9177163e43c9fd281b7238bc4e643c632c5e2d
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-04 18:50:51 +0300

    Time: 120 ms (30.82%), Space: 20.9 MB (5.22%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,32 +4,26 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    
-    def merge(self, node1: ListNode, node2: ListNode) -> ListNode:
-        
-        if not (node1 and node2):
-            return node1 if node1 else node2
-
-        if node1.val > node2.val:
-            nextNode = node2.next
-            node2.next = self.merge(node1, nextNode)
-            return node2
-
-        else:
-            nextNode = node1.next
-            node1.next = self.merge(node2, nextNode)
-            return node1
-
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
-            return 
+            return None
 
-        head = lists[0]
-        for index in range(1, len(lists)):
-            head = self.merge(head, lists[index])
-        
-        return head
-            
-            
-        
+        valueToNodeDict = defaultdict(list)
+        values = []
+
+        for node in lists:
+            while node:
+                values.append(node.val)
+                valueToNodeDict[node.val].append(ListNode(node.val))
+                node = node.next
+
+        heapify(values)
+        dummy = ListNode()
+        curr = dummy
+
+        for _ in range(len(values)):
+            curr.next = valueToNodeDict[heappop(values)].pop()
+            curr = curr.next
+
+
+        return dummy.next
