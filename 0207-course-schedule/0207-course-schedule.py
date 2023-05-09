f60abca2d51d9966bf2c9e19d62aaa4931157491
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-09 11:43:58 +0300

    Time: 121 ms (14.81%), Space: 17.8 MB (21.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -7,23 +7,22 @@ class Solution:
         for a, b in prerequisites:
             graph[b].append(a)
             indegree[a] += 1
-        
+
         queue = deque()
-        for course, _indegree in indegree.items():
-            if _indegree == 0:
+        for course in range(numCourses):
+            if indegree[course] == 0:
                 queue.append(course)
-        
-        
+
         while queue:
             course = queue.popleft()
-            
+
             for neighbor in graph[course]:
                 indegree[neighbor] -= 1
                 if indegree[neighbor] == 0:
                     queue.append(neighbor)
         
-        for course, _indegree in indegree.items():
-            if _indegree != 0:
+        for course in range(numCourses):
+            if indegree[course] != 0:
                 return False
         
         return True
