3c4bbfe0242ce19c8b4172ff955b96f653673b2f
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-10 19:28:32 +0300

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.py
@@ -0,0 +1,56 @@
+
+from typing import List
+from collections import deque
+
+class Solution:
+    #Function to detect cycle in an undirected graph.
+	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+
+		degree = {node: len(adj[node]) for node in range(V)}
+		queue = deque()
+		visited = set()
+
+		for node in range(V):
+		    if degree[node] == 1:
+		        queue.append(node)
+		
+		while queue:
+		    curr = queue.popleft()
+		    visited.add(curr)
+
+		    for neighbor in adj[curr]:
+		        if not neighbor in visited:
+    		        degree[curr] = 0
+    		        degree[neighbor] -= 1
+    
+    		        if degree[neighbor] == 1:
+    		            queue.append(neighbor)
+
+        for node in range(V):
+            if degree[node] != 0:
+                return True
+
+        return False
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+
+	T=int(input())
+	for i in range(T):
+		V, E = map(int, input().split())
+		adj = [[] for i in range(V)]
+		for _ in range(E):
+			u, v = map(int, input().split())
+			adj[u].append(v)
+			adj[v].append(u)
+		obj = Solution()
+		ans = obj.isCycle(V, adj)
+		if(ans):
+			print("1")
+		else:
+			print("0")
+
+# } Driver Code Ends
