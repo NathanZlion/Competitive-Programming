cf8345adbcdafdd739fdf5154ceccefdda3ad3ef
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-17 11:55:58 +0300

    Time: 60 ms (18.22%), Space: 18.9 MB (28.18%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -3,23 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # reorder the liked list in a way the odds come first and evens come next
-        # hold the last even node and last odd node pointers
-        # every time you access the next element
-        # make the `last even\odd` pointer point to it as next then update the `last odd/even` pointer 
-        # to the node itself
-        
+
         if not head or not head.next or not head.next.next:
             return head
         
         last_odd = head
         last_even = head.next
-
-        first_odd = last_odd
         first_even = last_even
-        
+
         curr = last_even.next
         is_even = False
         
@@ -38,6 +32,6 @@ class Solution:
         last_odd.next = first_even
         last_even.next = None
 
-        return first_odd
+        return head
             
         
