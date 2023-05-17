a31e97a847704e0ce304003ff1f97cf315588172
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-17 17:45:38 +0300

    Time: 1871 ms (76.43%), Space: 107.2 MB (58.38%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,28 +1,20 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+        # make themselves the reperesentatives for themselves
+        reps = [i for i in range(n)]
+
+        def representative(node):
+            curr = node
+
+            while reps[curr] != curr:
+                curr = reps[curr]
+
+            return curr
+
+        for from_, to_ in edges:
+            reps[representative(from_)] = representative(to_)
+
+
+        return representative(source) == representative(destination)
         
-        # build the graph from the edges given
-        graph = {}
-        for edge in edges:
-            if not edge[0] in graph: graph[edge[0]] = []
-            if not edge[1] in graph: graph[edge[1]] = []
-            
-            graph[edge[0]].append(edge[1])
-            graph[edge[1]].append(edge[0])
-    
-        visited_nodes = set()
-
-        # depth first traversal to get to the destination from the source node
-        stack = [source]
-
-        while len(stack) > 0:
-            node = stack.pop()
-            visited_nodes.add(node)
-            if node == destination:
-                return True
-            
-            for neighbour in graph[node]:
-                if not neighbour in visited_nodes:
-                    stack.append(neighbour)
-            
-        return False
