2f22e42a69da6306602a3fbd63a3f0dea6dc8813
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-17 17:49:18 +0300

    Time: 1764 ms (96.21%), Space: 107.1 MB (60.31%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -11,10 +11,20 @@ class Solution:
                 curr = reps[curr]
 
             return curr
+        
+        def unify(node1, node2):
+            common_rep = representative(node2)
+            
+            while reps[node1] != node1:
+                next = reps[node1]
+                reps[node1] = common_rep
+                node1 = next
+            
+            reps[node1] = common_rep
 
+            
         for from_, to_ in edges:
-            reps[representative(from_)] = representative(to_)
-
+            unify(from_, to_)
 
         return representative(source) == representative(destination)
         
