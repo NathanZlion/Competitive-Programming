50fab54bfb5fdbda9a9d4f76b580b3d190ed7c80
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-18 17:45:04 +0300

    Time: 843 ms (93.64%), Space: 24.8 MB (19.09%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -1,60 +1,42 @@
 class Solution:
-    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
-        # start with all zeros
-        res = [[0] * k for _ in range(k)]
-        """
-        Make row and col graph from given edges, then traverse to get a
-        topologically sorted order. return the res at any time there is a cycle.
-        """
-        # make row graph
-        rowIndegree = defaultdict(int)
-        rowAdjList = defaultdict(list)
+    
+    def findTopologicalOrder(self, k, conditions):
+        indegree = defaultdict(int)
+        adjList = defaultdict(list)
 
-        for _above, _below in rowConditions:
-            rowIndegree[_below] += 1
-            rowAdjList[_above].append(_below)
-        
+        for _first, _second in conditions:
+            indegree[_second] += 1
+            adjList[_first].append(_second)
+
         # topological sort traversal
         queue = deque()
         for node in range(1, k+1):
-            if rowIndegree[node] == 0:
+            if indegree[node] == 0:
                 queue.append(node)
         
-        rowOrder = []
+        order = []
         while queue:
             curr = queue.popleft()
-            rowOrder.append(curr)
+            order.append(curr)
             
-            for neighbor in rowAdjList[curr]:
-                rowIndegree[neighbor] -= 1
-                if rowIndegree[neighbor] == 0:
+            for neighbor in adjList[curr]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
                     queue.append(neighbor)
+        
+        return order
+        
+        
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        # start with all zeros
+        res = [[0] * k for _ in range(k)]
+
+        rowOrder = self.findTopologicalOrder(k, rowConditions)
 
         if len(rowOrder) != k:
             return []
 
-        # make col graph
-        colIndegree = defaultdict(int)
-        colAdjList = defaultdict(list)
-
-        for _left, _right in colConditions:
-            colIndegree[_right] += 1
-            colAdjList[_left].append(_right)
-        
-        # topological sort traversal
-        for node in range(1, k+1):
-            if colIndegree[node] == 0:
-                queue.append(node)
-        
-        colOrder = []
-        while queue:
-            curr = queue.popleft()
-            colOrder.append(curr)
-            
-            for neighbor in colAdjList[curr]:
-                colIndegree[neighbor] -= 1
-                if colIndegree[neighbor] == 0:
-                    queue.append(neighbor)
+        colOrder = self.findTopologicalOrder(k, colConditions)
         
         if len(colOrder) != k:
             return []
