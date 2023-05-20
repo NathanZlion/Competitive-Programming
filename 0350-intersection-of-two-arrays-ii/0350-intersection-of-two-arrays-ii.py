fb356476bd0b129dde1591a5c8c9386094092fec
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-20 21:33:19 +0300

    Time: 61 ms (42.92%), Space: 16.6 MB (5.38%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -7,17 +7,17 @@ class Solution:
         ptr2 = 0
         
         while ptr1 < len(nums1) and ptr2 < len(nums2):
-            if nums1[ptr1] == nums2[ptr2]:
+            if nums1[ptr1] < nums2[ptr2]:
+                ptr1 += 1
+            
+            elif nums1[ptr1] > nums2[ptr2]:
+                ptr2 += 1
+        
+            else:
                 res.append(nums1[ptr1])
                 ptr1 += 1
                 ptr2 += 1
                 
-            elif nums1[ptr1] < nums2[ptr2]:
-                ptr1 += 1
-            
-            else:
-                ptr2 += 1
-        
         return res
                 
             
