786589f64f2a7f18226019c37a6bdcd8b9a204af
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-05-31 19:18:33 +0300

    Time: 49 ms (26.05%), Space: 16.3 MB (21.22%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        def isInbound(row, col):
+            return 0 <= row < m and 0 <= col < n
+        
+        res = [[0 for _ in range(n)] for _ in range(m)]
+        
+        res[0][0] = 1
+        def getVal(row, col):
+            if isInbound(row, col):
+                return res[row][col]
+
+            return 0
+        
+        for row in range(m):
+            for col in range(n):
+                res[row][col] += getVal(row-1, col) + getVal(row, col-1)
+
+
+        return res[-1][-1]        
+        
+        
