7d4c86df36aca8080e1c598818809a8b4654da95
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-03 21:57:34 +0300

    Time: 56 ms (25.51%), Space: 16.4 MB (92.04%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal) or len(s) < 2: return False
+
+        differingIndices = []
+        for index in range(len(s)):
+            if s[index]  != goal[index]:
+                differingIndices.append(index)
+
+        # are identical
+        if len(differingIndices) == 0:
+            return len(set(s)) < len(s)
+        
+        if len(differingIndices) == 2:
+            firstIdx = differingIndices[0]
+            secondIdx = differingIndices[1]
+            return s[firstIdx] == goal[secondIdx] and s[secondIdx] == goal[firstIdx]
+        
+        return False
