777eff095a6af0a03240ca5703dc1169f54ad6e5
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-14 01:37:27 +0300

    Time: 131 ms (17.44%), Space: 18.1 MB (36.51%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        def isInbound(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                top = grid[row-1][col] if isInbound(row-1, col) else None
+                left = grid[row][col-1] if isInbound(row, col-1) else None
+
+                if top == None and left == None:
+                    continue
+
+                elif top != None and left != None:
+                    grid[row][col] = grid[row][col] + min(top, left)
+
+                elif top != None:
+                    grid[row][col] = grid[row][col] + top
+
+                elif left != None:
+                    grid[row][col] = grid[row][col] + left
+
+
+        return grid[-1][-1]
