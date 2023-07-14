284116b9f41a7ebbd98851667c63da1f98a520cb
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-14 22:05:47 +0300

    Time: 60 ms (46.70%), Space: 16.5 MB (27.22%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+
+        if obstacleGrid[-1][-1] == 1 or obstacleGrid[0][0] == 1:
+            return 0
+
+        rowLength = len(obstacleGrid)
+        colLength = len(obstacleGrid[0])
+
+        def isInbound(row, col):
+            return 0 <= row < rowLength and 0 <= col < colLength
+
+        for row in range(rowLength):
+            for col in range(colLength):
+                if obstacleGrid[row][col] == 1:
+                    obstacleGrid[row][col] = -1
+
+        obstacleGrid[0][0] = 1
+
+        for row in range(rowLength):
+            for col in range(colLength):
+                if obstacleGrid[row][col] == -1:
+                    obstacleGrid[row][col] = 0
+                    continue
+
+                if isInbound(row -1, col):
+                    obstacleGrid[row][col] += obstacleGrid[row -1][col]
+
+                if isInbound(row, col-1):
+                    obstacleGrid[row][col] += obstacleGrid[row][col-1]
+
+        return obstacleGrid[-1][-1]
