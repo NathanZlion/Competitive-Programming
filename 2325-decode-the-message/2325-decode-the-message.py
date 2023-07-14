e1a4017930a44464858bf8f9d1de5edd102f33ea
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-14 04:41:37 +0300

    Time: 52 ms (51.36%), Space: 16.5 MB (23.21%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        keyHash = {" ": " "}
+        alphabets = list(string.ascii_lowercase)
+        index = 0
+
+        for char in key:
+            if char in keyHash:
+                continue
+            
+            keyHash[char] = alphabets[index]
+            index += 1
+            if len(keyHash) == 27:
+                break
+        
+        decoded = [keyHash[char] for char in message]
+
+        return "".join(decoded)
