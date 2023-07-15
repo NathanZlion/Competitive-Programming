29d2ad41a65b175a80128633f021e6023c87373b
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-15 19:44:59 +0300

    Time: 1206 ms (21.48%), Space: 203.9 MB (14.82%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -0,0 +1,43 @@
+from functools import cache
+from typing import List
+
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        events = sorted(events, key=lambda x: (x[0], x[1]))
+        print(events)
+
+        @cache
+        def nextPossibleIndex(startIndex: int, date: int) -> int:
+            left = startIndex
+            right = len(events)
+
+            while right > left + 1:
+                mid = (left + right) // 2
+
+                if events[mid][0] > date:
+                    right = mid
+                else:
+                    left = mid
+
+            return right
+
+        @cache
+        def maxVal(index: int, remainingUnattended: int) -> int:
+            if index >= len(events) or remainingUnattended == 0:
+                return 0
+
+            startDate, endDate, currVal = events[index]
+
+            # Ignore the current event
+            dontAttendValue = maxVal(index + 1, remainingUnattended)
+
+            # Attend the current event
+            nextIndex = nextPossibleIndex(index, endDate)
+            attendValue = currVal
+
+            if nextIndex < len(events):
+                attendValue += maxVal(nextIndex, remainingUnattended - 1)
+
+            return max(attendValue, dontAttendValue)
+
+        return maxVal(index = 0, remainingUnattended = k)
