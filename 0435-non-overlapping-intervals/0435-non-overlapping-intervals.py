497ff8a683c8c585a8788fd32a2d3d377aadd296
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-19 15:57:57 +0300

    Time: 2602 ms (5.02%), Space: 58.3 MB (12.86%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,22 +3,20 @@ class Solution:
 
         # sort the intervals to get a time line
         intervals.sort()
-
         memo = {}
-        
+        total_intervals = len(intervals)
+
         def next_nonoverlapping(start_index, end_date):
             left = start_index
-            right = len(intervals)
-            
-            # right points to possible date
+            right = total_intervals
 
+            # right points to possible date
             while right > left + 1:
                 mid = left + ((right - left) // 2)
                 curr_start_date, _ = intervals[mid]
                 
                 if curr_start_date >= end_date:
                     right = mid
-                
                 else:
                     left = mid
 
@@ -28,13 +26,12 @@ class Solution:
         def traverse(index):
             if index in memo:
                 return memo[index]
-            
-            if index >= len(intervals):
+
+            if index >= total_intervals:
                 return 0
 
             _, end = intervals[index]
 
-            # consider the current interval
             add_curr = 1 + traverse(next_nonoverlapping( index, end))
             ignore_curr = traverse(index+1)
 
@@ -42,10 +39,7 @@ class Solution:
 
             return memo[index]            
 
+
         max_intervals = traverse(0)
 
-        # to get the minimum number of intervals that have to be removed 
-        # to attain a non-overlapping intervals subtract the maximum possible 
-        # number of intervals non overlapping intervals
-
-        return len(intervals) - max_intervals
+        return total_intervals - max_intervals
