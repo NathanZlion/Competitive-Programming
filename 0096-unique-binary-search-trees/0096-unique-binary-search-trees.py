91d712f64410407a51b16e3448399221205a576d
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-22 20:56:35 +0300

    Time: 47 ms (52.08%), Space: 16.4 MB (32.21%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,11 +1,11 @@
 class Solution:
-    def numTrees(self, n: int) -> int:        
-        # memo = {}
-        @cache
+    def numTrees(self, n: int) -> int:
+        # number_of_nodes : number_of_possibilities
+        memo = {0: 1, 1: 1}
+        
         def backtrack(left: int, right: int) -> int:
-            # if (right-left) in memo: return memo[right-left]
-            if right - left < 2:
-                return 1
+            if (right-left) in memo:
+                return memo[right-left]
 
             possibilities = 0
 
@@ -14,6 +14,8 @@ class Solution:
                 right_subtree = backtrack(left=node+1, right=right)
                 curr_possibility = (left_subtree * right_subtree)
                 possibilities += curr_possibility
+
+            memo[right-left] = possibilities
 
             return possibilities
 
