dd5dce32a04803a22f32c5c2df4c95887a492277
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-22 20:54:35 +0300

    Time: 53 ms (16.35%), Space: 16.3 MB (70.17%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,41 +1,20 @@
 class Solution:
-    def numTrees(self, n: int) -> int:
-        match n:
-            case 1:
+    def numTrees(self, n: int) -> int:        
+        # memo = {}
+        @cache
+        def backtrack(left: int, right: int) -> int:
+            # if (right-left) in memo: return memo[right-left]
+            if right - left < 2:
                 return 1
-            case 2:
-                return 2
-            case 3:
-                return 5
-            case 4:
-                return 14
-            case 5:
-                return 42
-            case 6:
-                return 132
-            case 7:
-                return 429
-            case 8:
-                return 1430
-            case 9:
-                return 4862
-            case 10:
-                return 16796
-            case 11:
-                return 58786
-            case 12:
-                return 208012
-            case 13:
-                return 742900
-            case 14:
-                return 2674440
-            case 15:
-                return 9694845
-            case 16:
-                return 35357670
-            case 17:
-                return 129644790
-            case 18:
-                return 477638700
-            case 19:
-                return 1767263190
+
+            possibilities = 0
+
+            for node in range(left, right):
+                left_subtree = backtrack(left=left, right=node)
+                right_subtree = backtrack(left=node+1, right=right)
+                curr_possibility = (left_subtree * right_subtree)
+                possibilities += curr_possibility
+
+            return possibilities
+
+        return backtrack (left=0, right=n)
