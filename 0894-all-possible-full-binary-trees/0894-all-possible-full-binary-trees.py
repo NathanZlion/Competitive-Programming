ba0067322531b68513109748ab52eb84ff2fc9b7
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-23 10:44:26 +0300

    Time: 176 ms (83.81%), Space: 21.9 MB (37.29%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.py
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.py
@@ -5,33 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    @cache
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
         """
         # return a list of treenodes, that are roots of the possible binary trees.
         # I am thinking of using recursive functional call with remaining nodes as an argument
         """
 
-        @cache
-        def build_tree(remaining) -> List[Optional[TreeNode]]:
-            if remaining == 1:
-                return [TreeNode()]
+        if n == 1:
+            return [TreeNode()]
+        
+        if n == 0 or n == 2:
+            return []
+        
+        res = []        
+        for i in range(n):
+            left_subtree = self.allPossibleFBT(i)
+            right_subtree = self.allPossibleFBT(n-i-1)
             
-            if remaining in [0, 2]:
-                return []
+            for left_child in left_subtree:
+                for right_child in right_subtree:
+                    res.append(TreeNode(0, left_child, right_child))
 
-            res = []
-            
-            for i in range(remaining):
-                left_remaining = i
-                right_remaining = remaining-i-1
 
-                left_subtree = build_tree(left_remaining)
-                right_subtree = build_tree(right_remaining)
-                for left_child in left_subtree:
-                    for right_child in right_subtree:
-                        root = TreeNode(0, left_child, right_child)
-                        res.append(root)
-
-            return res
-
-        return build_tree(n)
+        return res
