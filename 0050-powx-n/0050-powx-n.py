96324b1c054bfad005fab6d4c55c652791c2701e
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-07-24 09:13:37 +0300

    Time: 40 ms (89.54%), Space: 16.6 MB (7.14%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,10 +1,19 @@
 class Solution:
+    @cache
     def myPow(self, x: float, n: int) -> float:
-        
+
         if n == 0:
             return 1
 
+        if n == 1:
+            return x
+
         if n < 0:
-            return 1/ self.myPow(x,abs(n))
-        
-        return x* self.myPow(x, n-1) if n%2 else self.myPow(x*x, n//2)
+            return 1 / self.myPow(x, -n)
+
+        # even power
+        if n % 2 == 0:
+            return self.myPow(x, n//2) * self.myPow(x, n//2)
+
+        # odd power
+        return self.myPow(x, n-1) * x
