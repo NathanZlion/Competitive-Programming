8a7d63cf138a250752b3d5e9eb8e4840b4384c9c
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-08-01 16:26:18 +0300

    Time: 75 ms (85.93%), Space: 37.4 MB (74.20%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
@@ -1,35 +1,32 @@
 public class Solution {
-    public string MergeAlternately(string word1, string word2) {
-        // start from the first Word
-        bool FirstTurn = true;
-        
+    public string MergeAlternately(string word1, string word2) {        
         int ptr1 = 0;
         int ptr2 = 0;
-        char[] chars = new char[word1.Length + word2.Length];
+
+        char[] merged = new char[word1.Length + word2.Length];
+        int ptr = 0;
 
         while (ptr1 < word1.Length && ptr2 < word2.Length){
-            if (FirstTurn){
-                chars[ptr1 + ptr2] = word1[ptr1];
-                ptr1++;
-            }
-            else
-            {
-                chars[ptr1 + ptr2] = word2[ptr2];
-                ptr2++;
-            }
-            FirstTurn = !FirstTurn;
-        }
-
-        while (ptr1 < word1.Length){
-            chars[ptr1 + ptr2] = word1[ptr1];
+            merged[ptr] = word1[ptr1];
+            ptr++;
+            merged[ptr] = word2[ptr2];
+            ptr++;
             ptr1++;
-        }
-
-        while (ptr2 < word2.Length){
-            chars[ptr1 + ptr2] = word2[ptr2];
             ptr2++;
         }
 
-        return new string(chars);
+        while (ptr1 < word1.Length){
+            merged[ptr] = word1[ptr1];
+            ptr1++;
+            ptr++;
+        }
+
+        while (ptr2 < word2.Length){
+            merged[ptr] = word2[ptr2];
+            ptr2++;
+            ptr++;
+        }
+
+        return new string(merged);
     }
 }
