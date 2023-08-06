84c6e4be3e037c49cc6f85ba223caf06c89ef847
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-08-06 14:57:12 +0300

    Time: 42 ms (79.57%), Space: 16.4 MB (10.37%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,37 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        # need a memo dictionary
         memo = {}
-        
-        # base case out of range returns 0
-        number_of_homes = len(nums)
-        
-        def max_loot(house_number, house_zero = False):
-            nonlocal number_of_homes
-            
-            if house_number >= number_of_homes:
-                return 0
-            
-            # if started at house zero avoid visiting the last home as it is adjecant to house 0
-            if house_zero and house_number == number_of_homes - 1:
+
+        def max_loot(house_num, end_house):
+
+            if house_num > end_house:
                 return 0
 
-            if house_number in memo:
-                return memo[house_number]
+            if house_num not in memo:
+                memo[house_num] = nums[house_num]
+                memo[house_num] += max(max_loot(house_num+2, end_house), max_loot(house_num+3, end_house))
 
-            memo[house_number] = nums[house_number] + max(max_loot(house_number + 2, house_zero), max_loot(house_number + 3, house_zero))
-
-            return memo[house_number]
+            return memo[house_num]
 
         if len(nums) < 4:
             return max(nums)
-        
-        first_possibility = max_loot(0, True)
-        memo.clear()
-        second_possibility = max_loot(1, False)
-        memo.clear()
-        third_possibility = max_loot(2, False)
 
+        first_possiblilty = max_loot(0, len(nums)-2)
+        memo.clear()
+        second_possibility = max_loot(1, len(nums)-1)
+        memo.clear()
+        third_possibility = max_loot(2, len(nums)-1)
 
-        return max(first_possibility, second_possibility, third_possibility)
+        return max(first_possiblilty, second_possibility, third_possibility)
