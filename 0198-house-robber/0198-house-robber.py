91fdb46fdc9c9daa81742065e4f16a6d67232e62
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-08-06 14:22:39 +0300

    Time: 35 ms (95.14%), Space: 16.2 MB (73.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,26 +1,13 @@
-class Solution:
-    
-    def __init__(self):
-        self.memo = {}
-        
+class Solution:        
     def rob(self, nums: List[int]) -> int:
         self.nums = nums
-        
-        return max(self.dp(0), self.dp(1))
 
+        return max(self.dp(0), self.dp(1))
+    
+    @lru_cache
     def dp(self, index):
-        # if already in memo return that
-        if index in self.memo:
-            return self.memo[index]
-        
         if index > len(self.nums)-1:
             return 0
 
         # try moving back 2 steps back and 3 steps back
-        max_possibility = max(self.dp(index+2), self.dp(index+3))
-        max_possibility += self.nums[index]
-        self.memo[index] = max_possibility
-        return max_possibility
-        
-        
-        
+        return max(self.dp(index+2), self.dp(index+3)) + self.nums[index]
