8c2857900dde80a05d329b5bddd8bd9a9c2bbfd9
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-08-12 15:35:49 +0300

    Time: 46 ms (90.91%), Space: 16.5 MB (27.79%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,28 +1,31 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
 
-        if obstacleGrid[-1][-1] == 1 or obstacleGrid[0][0] == 1:
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+
+        # edge cases
+        if 1 in [obstacleGrid[0][0], obstacleGrid[m-1][n-1]]:
             return 0
 
-        rowLength = len(obstacleGrid)
-        colLength = len(obstacleGrid[0])
+        # add padding to the right and bottom
+        for row in obstacleGrid:
+            row.append(0)
+        obstacleGrid.append([0 for _ in range(n+1)])
+        obstacleGrid[0][0] = 1
 
-        def isInbound(row, col):
-            return 0 <= row and 0 <= col
+        for col in range(1, n):
+            if obstacleGrid[0][col] == 1:
+                obstacleGrid[0][col] = 0
+            else:
+                obstacleGrid[0][col] = obstacleGrid[0][col-1] + obstacleGrid[-1][col]
 
-        obstacleGrid[0][0] = -1
-
-        for row in range(rowLength):
-            for col in range(colLength):
+        for row in range(1, m):
+            for col in range(n):
                 if obstacleGrid[row][col] == 1:
                     obstacleGrid[row][col] = 0
-                    continue
-
-                if isInbound(row -1, col):
-                    obstacleGrid[row][col] += obstacleGrid[row -1][col]
-
-                if isInbound(row, col-1):
-                    obstacleGrid[row][col] += obstacleGrid[row][col-1]
+                else:
+                    obstacleGrid[row][col] = obstacleGrid[row-1][col] + obstacleGrid[row][col-1]
 
 
-        return -obstacleGrid[-1][-1]
+        return obstacleGrid[m-1][n-1]
