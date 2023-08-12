6eff71bf3f246d53901de31254a76c87f69040a4
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-08-12 15:48:14 +0300

    Time: 2404 ms (34.37%), Space: 24.1 MB (21.17%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,22 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-
         n = len(s)
-        longestSubstring = ""
+        dp = [[False] * n for _ in range(n)]
+        ans = [0, 0]
         
-        def isPalindrome(left, right):
-            while right > left:
-                if s[right]  != s[left]:
-                    return False
-                
-                right -= 1
-                left += 1
+        # one letter is a palindrome for sure
+        for i in range(n):
+            dp[i][i] = True
+        
+        # for 2 letters to be palindrome we need them to be same
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                ans = [i, i + 1]
 
-            return True
+        for length in range(3, n+1):
+            for left in range(n - length + 1):
+                right = left + length - 1
+                if s[left] == s[right] and dp[left + 1][right - 1]:
+                    dp[left][right] = True
+                    ans = [left, right]
 
-        for length in range(len(s), 0, -1):
-            for start in range(0, len(s) - length + 1):
-                if isPalindrome(start, start + length - 1):
-                    return s[start:start+length]
-
-        return ""
+        left, right = ans
+        return s[left: right + 1]
