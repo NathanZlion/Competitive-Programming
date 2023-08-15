4c788b5028d31b45ea3329ec94d95e1c4e0f9948
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-08-15 16:48:47 +0300

    Time: 45 ms (70.03%), Space: 16.4 MB (14.86%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -1,28 +1,31 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def partition(self, head, x):
-        
-        curr = head
-        before = []
-        after = []
-        while curr:
-            if curr.val < x:
-                before.append(curr.val)
+class Solution:
+    @staticmethod
+    def traverse(head: Optional[ListNode], x: int):
+        before, after = [], []
+        node = head
+        while node:
+            if node.val < x:
+                before.append(node)
             else:
-                after.append(curr.val)
-            
-            curr = curr.next
-        
+                after.append(node)
+            node = node.next
+
+        return before, after
+
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        before, after = Solution.traverse(head, x)
         before.extend(after)
-        
-        curr = head
-        for index in range(len(before)):
-            curr.val = before[index]
-            curr = curr.next
-        
-        return head
-                
+        dummyHead = ListNode()
+        curr = dummyHead
+
+        for node in before:
+            curr.next = node
+            curr = node
+
+        curr.next = None
+        return dummyHead.next
