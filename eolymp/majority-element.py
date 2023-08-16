ded5c9efff9ff5bc126a0a4feb283edc5a422d43
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-08-16 16:55:02 +0300

    Uploaded By Mirkusve

---
+++ eolymp/majority-element.py
@@ -0,0 +1,1 @@
+class Solution:    def majorityElement(self, nums: List[int]) -> int:        freq = defaultdict(int)        for num in nums:            freq[num] += 1        for num, count in freq.items():            if count > len(nums)//2:                return num
