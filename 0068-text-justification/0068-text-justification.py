b3042accb6f81de9fef9810e03573c79c5bced04
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-08-24 11:52:16 +0300

    Time: 36 ms (88.28%), Space: 16.6 MB (31.04%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,33 +1,35 @@
 class Solution:
+    @staticmethod
+    def leftRightJustify(words: List[str], maxlineLength: int) -> str:
+        if len(words) == 1:
+            return words[0]+(" "*(maxlineLength -len(words[0])))
+
+        numSpaces = (len(words)-1)
+        wordsLength = sum(len(word) for word in words)
+        spaceLength = maxlineLength - wordsLength
+        base = spaceLength // numSpaces
+        spaces = [base] * numSpaces
+        remainder = spaceLength % numSpaces
+
+        for index in range(remainder):
+            spaces[index] += 1
+
+        res = []
+        if len(words) > 0:
+            res.append(words[0])
+            for index in range(numSpaces):
+                res.append(" "*spaces[index])
+                res.append(words[index+1])
+
+        return "".join(res)
+    
+    @staticmethod
+    def leftJustify(words: List[str], maxlineLength: int) -> str:
+        spaceJoinedWords = " ".join(words)
+        rightSpaces = " " * (maxlineLength - len(spaceJoinedWords))
+        return spaceJoinedWords + rightSpaces
+
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        def justify(words :List[str]) -> str:
-            if len(words) == 1:
-                return words[0]+(" "*(maxWidth -len(words[0])))
-
-            numSpaces = (len(words)-1)
-            wordsLength = sum(len(word) for word in words)
-            spaceLength = maxWidth - wordsLength
-            base = spaceLength // numSpaces
-            spaces = [base] * numSpaces
-            remainder = spaceLength % numSpaces
-
-            for index in range(remainder):
-                spaces[index] += 1
-
-            res = []
-            if len(words) > 0:
-                res.append(words[0])
-                for index in range(numSpaces):
-                    res.append(" "*spaces[index])
-                    res.append(words[index+1])
-            
-            return "".join(res)
-
-        def justifyLeft(wordLst: List[str]) -> str:
-            spaceJoinedWords =" ".join(wordLst)
-            rightSpaces = " " * (maxWidth - len(spaceJoinedWords))
-            return spaceJoinedWords + rightSpaces
-
         lines = []
         wordLst = []
         currWordsLength = 0
@@ -46,12 +48,10 @@ class Solution:
         if wordLst:
             lines.append(wordLst.copy())
 
-
         res = []
         for index in range(len(lines)-1):
-            res.append(justify(lines[index]))
-
+            res.append(Solution.leftRightJustify(lines[index], maxWidth))
         if lines:
-            res.append(justifyLeft(lines[-1]))
+            res.append(Solution.leftJustify(lines[-1], maxWidth))
 
         return res
