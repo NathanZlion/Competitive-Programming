3e07b87db51e0b656b9a19ab86154a90bf5b159a
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-09-01 23:10:18 +0300

    Time: 83 ms (61.74%), Space: 23 MB (94.90%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,17 +1,15 @@
 class Solution:
     def numberOfBits(self, num : int) -> int:
+        if num in self.memo:
+            return self.memo[num]
 
-        if num in self.memo: return self.memo[num]
-
-        return 1 + self.numberOfBits(num>>1) if num % 2 else self.numberOfBits(num>>1)
-
+        if num % 2:
+            return self.numberOfBits(num>>1) + 1
+        else:
+            return self.numberOfBits(num>>1)
 
     def countBits(self, n: int) -> List[int]:
-
-        self.memo = {
-            0:0,
-            1:1,
-        }
+        self.memo = {0:0, 1:1}
 
         arr = [0 for _ in range(n+1)]
 
