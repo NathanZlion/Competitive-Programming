d233193fd8a4a5001ad9e455838115ec688d0702
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-09-03 14:05:52 +0300

    Time: 44 ms (42.03%), Space: 16.3 MB (76.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,24 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-
-        def isInbound(row, col):
-            return 0 <= row < m and 0 <= col < n
-
-        # starting with a 0 populated grid (matrix)
-        res = [[0 for _ in range(n)] for _ in range(m)]
-
-        res[0][0] = 1
-
-        def getVal(row, col):
-            if isInbound(row, col):
-                return res[row][col]
-
-            return 0
-
+        grid = [[0]*n for _ in range(m)]
+        grid[0][0] = 1
+        
+        def is_inbound(row: int, col: int) -> bool:
+            return 0 <= col < n and 0 <= row < m
+        
         for row in range(m):
             for col in range(n):
-                # sum of direct left and direct up neighbor's moves
-                res[row][col] += getVal(row-1, col) + getVal(row, col-1)
+                if is_inbound(row-1, col):
+                    grid[row][col] +=  grid[row-1][col]
+                
+                if is_inbound(row, col-1):
+                    grid[row][col] +=  grid[row][col-1]
 
-
-        return res[-1][-1]
+        return grid[m-1][n-1]
