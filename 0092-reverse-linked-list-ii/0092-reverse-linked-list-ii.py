fff7dccf81c437fe3c08f329f7fb586ce243b017
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-09-07 21:28:17 +0300

    Time: 45 ms (33.89%), Space: 16.5 MB (31.02%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -1,48 +1,37 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        
-        if left == right:
-            return head
-
-        dummyNode = ListNode(-1, head)
-        curr = dummyNode
-
-        for _ in range(left-1):
-            curr = curr.next
-        
-        start = curr
-        reverseStart = start.next
-
-        for _ in range(right - left + 1):
-            curr = curr.next
-        
-        reverseEnd = curr
-        end = reverseEnd.next
-        
-        # reverse what's in between
-        dummy = ListNode(-5, reverseStart)
-        prev = dummy
-        curr = reverseStart
-        nxt = reverseStart.next
-        reverseEnd.next = None
+    @staticmethod
+    def __reverse(prev: Optional[ListNode], curr: Optional[ListNode]) -> None:
+        nxt = curr.next
 
         while curr:
-            nxt = curr.next
             curr.next = prev
             prev = curr
             curr = nxt
+            if not curr:
+                break
+            nxt = curr.next
 
-        start.next = reverseEnd
-        reverseStart.next = end
+
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummyHead = ListNode(-1, head)
+        curr = dummyHead
+
+        for _ in range(left-1):
+            curr = curr.next
+
+        preLeft = curr
+        curr = curr.next
+        leftPtr = curr
         
-        return dummyNode.next
+        for _ in range(right-left):
+            curr = curr.next
         
-            
+        rightPtr = curr
+        nxtRight = rightPtr.next
+        rightPtr.next = None
         
+        Solution.__reverse(prev=preLeft, curr=leftPtr)
+        preLeft.next = rightPtr
+        leftPtr.next = nxtRight
         
-        
+        return dummyHead.next
