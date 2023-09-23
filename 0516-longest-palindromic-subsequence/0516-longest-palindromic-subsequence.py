7632d158995b0a842b4fcef46fd661d4996f78c8
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-09-23 14:05:47 +0300

    Time: 710 ms (92.71%), Space: 252.9 MB (10.47%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,26 +1,22 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        memo = {}
         n = len(s)
 
-        def palindromeLength(left = 0, right = n-1):
-            # base cases
-            if (left, right) in memo:
-                return memo[(left, right)]
-
+        @cache
+        def palindromeLength(left, right):
             if right == left:
                 return 1
 
-            if right - left == 1:
+            elif right == left + 1:
                 return 2 if s[left] == s[right] else 1
 
-            if s[left] == s[right]:
-                memo[(left, right)] = 2 + palindromeLength(left+1, right-1)
+            elif s[left] == s[right]:
                 return 2 + palindromeLength(left+1, right-1)
+
             else:
-                memo[(left, right)] = max(palindromeLength(left+1, right),palindromeLength(left, right-1))
+                return max (
+                        palindromeLength(left+1, right),
+                        palindromeLength(left, right-1)
+                    )
 
-            return memo[(left, right)]
-
-
-        return palindromeLength()
+        return palindromeLength(left = 0, right= len(s)-1)
