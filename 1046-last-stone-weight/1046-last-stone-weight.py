1b32b31b7bb103c4e9dc77637ec533a434761bb4
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-09-25 17:21:18 +0300

    Time: 49 ms (5.98%), Space: 16.3 MB (60.52%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,5 +1,6 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        
         # invert the sign to make a max heap
         stones = [-stone for stone in stones]
         heapify(stones)
@@ -8,7 +9,7 @@ class Solution:
             stone1 = heappop(stones)
             stone2 = heappop(stones)
 
-            if -stone1 > -stone2:
+            if stone2 - stone1 > 0:
                 heappush(stones, stone1 - stone2)
 
         return -stones[0] if stones else 0
