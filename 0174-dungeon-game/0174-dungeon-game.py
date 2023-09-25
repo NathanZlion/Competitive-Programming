448bc5d08e451a67b5e97fca3eb8cfe038231777
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-09-25 17:06:52 +0300

    Time: 85 ms (15.03%), Space: 19.1 MB (12.67%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -0,0 +1,19 @@
+class Solution:
+
+        
+        
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        rows, cols = len(dungeon), len(dungeon[0])
+        dp = defaultdict(lambda : -inf)
+        dp[(rows,cols-1)] = 0 
+        
+    
+        for row in range(rows-1,-1,-1):
+            for col in range(cols-1,-1,-1):
+                
+                dp[(row,col)] = max(dp[(row+1,col)],dp[(row,col+1)])
+                dp[(row,col)] = min(dungeon[row][col] + dp[(row,col)],0)
+        
+        return abs(dp[(0,0)] )+ 1
+                
+        
