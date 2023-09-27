3b079db895f0937bd361a513c71bc0399de0d0ed
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-09-27 13:11:41 +0300

    Time: 54 ms (5.57%), Space: 16.3 MB (86.41%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,46 @@
+class TrieNode:
+    def __init__(self, val: str):
+        self.val = val
+        self.children = {}
+        self.isEndOfWord = False
+        self.wordCount = 1
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode('*')
+    
+    def insertWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode(char)
+            else:
+                curr.children[char].wordCount += 1
+
+            curr = curr.children[char]
+        
+        curr.isEndOfWord = True    
+    
+    def insertWords(self, words: List[str]) -> None:
+        for word in words:
+            self.insertWord(word)
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        trie = Trie()
+        trie.insertWords(strs)
+        curr = trie.root
+        N = len(strs)
+        res = []
+
+        while curr.children.__len__() == 1:
+            child = next(iter(curr.children))
+            nextNode = curr.children[child]
+
+            if nextNode.wordCount != N:
+                break
+
+            res.append(child)
+            curr = nextNode
+        
+        return ''.join(res)
