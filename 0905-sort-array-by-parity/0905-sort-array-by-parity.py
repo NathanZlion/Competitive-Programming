0e18bfd343834edd219fcc0633292a09bd487adb
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-09-28 20:46:10 +0300

    Time: 83 ms (21.04%), Space: 17 MB (96.40%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -3,13 +3,15 @@ class Solution(object):
         newArr = [num for num in nums]
         p_even = 0
         p_odd = len(nums)-1
+
         for num in nums:
+            # num is odd
             if num % 2:
                 newArr[p_odd] = num
                 p_odd -= 1
+            # num is even
             else:
                 newArr[p_even] = num
                 p_even += 1
         
         return newArr
-        
