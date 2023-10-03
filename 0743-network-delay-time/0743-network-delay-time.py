197f0b6038f1b84faa7d27e93650f39a56c35013
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-03 18:35:22 +0300

    Time: 424 ms (43.88%), Space: 18.8 MB (33.96%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,22 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # build adjList from weights.
+        adjList = defaultdict(list)
+        for u, v, w in times:
+            adjList[u].append((w, v))
+
+        minTimes = [inf for _ in range(n)]
+        minTimes[k-1] = 0
+        priorityQueue = [[0, k]]
+        while priorityQueue:
+            dist, node = heappop(priorityQueue)
+            
+            for weight, neighborNode in adjList[node]:
+                if dist + weight < minTimes[neighborNode - 1]:
+                    minTimes[neighborNode - 1] = dist + weight
+                    heappush(priorityQueue, (dist + weight, neighborNode))
+
+        minTimeToReachAll = max(minTimes)
+        print(minTimes)
+
+        return -1 if minTimeToReachAll == inf else minTimeToReachAll
