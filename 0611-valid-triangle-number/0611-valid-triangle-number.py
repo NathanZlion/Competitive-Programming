2af1d1e4645c1a894b77deb10766c0dd21c97f08
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-03 20:19:50 +0300

    Time: 948 ms (44.56%), Space: 16.4 MB (37.40%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,15 +1,18 @@
-from bisect import bisect_left, bisect_right
-from typing import List
-
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
         numOfValidTriangles = 0
         n = len(nums)
+        for k in range(n-1, 1, -1):
+            i = 0
+            for j in range(k-1, 0, -1):
+                if i >= j:
+                    break
 
-        for i in range(n-2):
-            for j in range(i+1, n-1):
-                k = bisect_right(nums, nums[i]+nums[j]-1)
-                numOfValidTriangles += max(k-j-1, 0)
+                while j > i and nums[i]+nums[j] <= nums[k]:
+                    i += 1
 
+                if nums[i]+nums[j] > nums[k]:
+                    numOfValidTriangles += (j-i)
+        
         return numOfValidTriangles
