427a31732505cfc4ae95cc5718439a1a879df55b
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-05 07:22:57 +0300

    Time: 35 ms (78.56%), Space: 16.2 MB (79.88%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -2,4 +2,4 @@ from math import ceil, floor, sqrt
 
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        return ceil(sqrt(num)) == floor(sqrt(num))
+        return int(sqrt(num)) == sqrt(num)
