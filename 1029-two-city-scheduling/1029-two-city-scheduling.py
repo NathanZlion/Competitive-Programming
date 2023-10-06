e6fa02ed971dc50fa750e7aae3f00730747347da
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-06 15:48:14 +0300

    Time: 71 ms (7.61%), Space: 18 MB (5.01%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,28 +1,23 @@
 
 class Solution:
-#     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-#         totalPeople = len(costs)
-
-#         @cache
-#         def dp(index: int, leftA: int, leftB: int):
-#             if leftA < 0 or leftB < 0:
-#                 return inf
-
-#             if index == totalPeople:
-#                 return 0
-
-#             costOfA, costOfB = costs[index]
-
-#             # choose to send to City A or City B
-#             return min(
-#                 costOfA + dp(index + 1, leftA - 1, leftB),
-#                 costOfB + dp(index + 1, leftA, leftB - 1),
-#             )
-
-#         return dp(0, totalPeople//2, totalPeople//2)
-
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        costs.sort(key=lambda cost: cost[0] - cost[1])
-        costs_for_A = sum([cost[0] for cost in costs[:len(costs) // 2]])
-        costs_for_B = sum([cost[1] for cost in costs[len(costs) // 2:]])
-        return costs_for_A + costs_for_B
+        totalPeople = len(costs)
+
+        @cache
+        def dp(index: int, leftA: int, leftB: int):
+            if leftA < 0 or leftB < 0:
+                return inf
+
+            if index == totalPeople:
+                return 0
+
+            costOfA, costOfB = costs[index]
+
+            # choose to send to City A or City B
+            return min(
+                costOfA + dp(index + 1, leftA - 1, leftB),
+                costOfB + dp(index + 1, leftA, leftB - 1),
+            )
+
+        return dp(0, totalPeople//2, totalPeople//2)
+    
