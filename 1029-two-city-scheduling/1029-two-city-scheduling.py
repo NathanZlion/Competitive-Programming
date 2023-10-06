0a6b9690bfe9436f50bc34b07a3d803b56405ad3
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-06 15:42:30 +0300

    Time: 71 ms (7.61%), Space: 17.8 MB (5.93%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        totalPeople = len(costs)
+
+        @cache
+        def dp(index: int, leftA: int, leftB: int):
+            if leftA < 0 or leftB < 0:
+                return inf
+
+            if index == totalPeople:
+                return 0
+
+            costOfA, costOfB = costs[index]
+
+            # choose to send to City A or City B
+            return min(
+                costOfA + dp(index + 1, leftA - 1, leftB),
+                costOfB + dp(index + 1, leftA, leftB - 1),
+            )
+
+        return dp(0, totalPeople//2, totalPeople//2)
