b1b46fc248979c8ea2ffcfa0e075a5e1968fdd14
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-09 18:32:43 +0300

    Time: 57 ms (5.98%), Space: 16.4 MB (18.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,8 +1,61 @@
+class Hash:
+    MOD = 10**9+7
+    a = 26
+    def __init__(self, val: int = 0, count: int = 0):
+        self.val = val
+        self.count = count
+
+    def addFirst(self, char: str):
+        addCharInt = Hash.charToInt(char)
+        self.val *= Hash.a
+        self.val += addCharInt
+        self.val %= Hash.MOD
+        self.count += 1
+    
+    def pollLast(self, char: str) -> None:
+        popCharInt = Hash.charToInt(char)
+        self.val -= (popCharInt * Hash.a**(self.count-1))
+        self.val %= Hash.MOD
+        self.count -= 1
+
+    @staticmethod
+    def charToInt(char: str):
+        return ord(char) - ord('a') + 1
+
+    @staticmethod
+    def hash(word: str) -> 'Hash':
+        n = len(word)
+        val = 0
+        a = 26
+        for i in range(n):
+            val *= a
+            val += Hash.charToInt(word[i])
+            val %= Hash.MOD
+
+        return Hash(val, n)
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        n = len(haystack)
+        k = len(needle)
+        
+        if k > n:
+            return -1
 
-        for index in range(len(haystack) - len(needle)+1):
-            if haystack[index:index+len(needle)] == needle:
-                return index
+        if k == 0:
+            return 0
 
+        targetHashVal = Hash.hash(needle).val
+        runningHash = Hash.hash(haystack[:k])
+
+        if runningHash.val == targetHashVal and haystack[:k] == needle:
+            return 0
+        
+        for i in range(1, n - k + 1):
+            runningHash.addFirst(haystack[i + k - 1])
+            runningHash.pollLast(haystack[i - 1])
+            if runningHash.val == targetHashVal:
+                if haystack[i:i + k] == needle:
+                    return i
+        
         return -1
