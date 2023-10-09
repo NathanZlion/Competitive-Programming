a3d84a93528909b10914a3f8d030a3145484fa3e
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-09 11:12:05 +0300

    Time: 326 ms (40.53%), Space: 20.7 MB (23.06%) - LeetHub

---
+++ 0475-heaters/0475-heaters.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        def closestHeaterDistance(houseLocation: int) -> int:
+            """
+            finds 2 towers that are immediately to the left and right of it.
+            """
+            left = -1
+            right = len(heaters)
+            
+            while right > left + 1:
+                mid = (left + right) // 2
+                
+                # heater is to the left of the target house
+                if houseLocation > heaters[mid]:
+                    left = mid
+                else:
+                    right = mid
+
+            left = max(left, 0)
+            right = min(right, len(heaters)-1)
+
+            return min(abs(heaters[left] - houseLocation), abs(heaters[right] - houseLocation))                
+
+        houses.sort()
+        heaters.sort()
+        minR = 0
+        for house in houses:
+            minR = max(minR, closestHeaterDistance(house))
+        
+        return minR
