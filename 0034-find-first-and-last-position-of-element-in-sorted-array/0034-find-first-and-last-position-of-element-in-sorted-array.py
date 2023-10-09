38a30a2b1beb2602bb3db24ccfd74bcb67f541ca
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-09 07:27:15 +0300

    Time: 75 ms (94.63%), Space: 17.6 MB (84.76%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,20 @@
 class Solution:
-    def startingPosition(self):
+    @staticmethod
+    def getPosition(nums: List[int], target: int, endPosition: bool) -> int:
         low = -1
-        high = len(self.nums)
-        
+        high = len(nums)
+
         while high > low+1:
             mid = (high + low)//2
-            if self.nums[mid] >= self.target:
+            if (endPosition and nums[mid] > target) or ((not endPosition) and nums[mid] >= target):
                 high = mid
             else:
                 low = mid
 
-        return -1 if high == len(self.nums) or self.nums[high] != self.target else high
+        if endPosition:
+            return -1 if (low == -1 or nums[low] != target) else low
 
-
-    def endingPosition(self):
-        low = -1
-        high = len(self.nums)
-        
-        while high > low+1:
-            mid = (high + low)//2
-            if self.nums[mid] > self.target:
-                high = mid
-            else:
-                low = mid
-
-        return -1 if low == -1 or self.nums[low] != self.target else low
-    
+        return -1 if (high == len(nums) or nums[high] != target) else high
 
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        self.nums = nums
-        self.target = target
-        return [self.startingPosition(), self.endingPosition()]
+        return [Solution.getPosition(nums, target, False), Solution.getPosition(nums, target, True)]
