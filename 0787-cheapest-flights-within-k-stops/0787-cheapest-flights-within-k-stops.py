d1eb1bdc01ca44c0ea413f1b3aedf87523f87b9c
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-10 14:31:40 +0300

    Time: 205 ms (22.11%), Space: 19 MB (9.03%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,19 +1,17 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = defaultdict(list)
-
-        # construct the adjecency list using the edges (flights) given
         for city_from, city_to, flight_price in flights:
             graph[city_from].append((city_to, flight_price))
         
-        priority_queue = []
+        visited = set()
         # storing the price, node, num_of_stops
         priority_queue = [(0, src, 0)]
-        visited = set()
-        
+        min_dist = defaultdict(lambda : inf)
+        min_dist[src] = 0
+
         while priority_queue:
             price, curr_node, stops = heappop(priority_queue)
-            
             if (curr_node, stops) in visited:
                 continue
 
@@ -23,6 +21,9 @@ class Solution:
                 return price
 
             for next_city, cost in graph[curr_node]:
+                if min_dist[next_city] < cost + price + cost:
+                    continue
+
                 if next_city == src or next_city == dst:
                     heappush(priority_queue, (price + cost, next_city, stops))
 
