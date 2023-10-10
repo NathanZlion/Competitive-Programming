d57da5856bec2edd75a15491793826ceb506308d
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-10 19:06:14 +0300

    Time: 53 ms (97.16%), Space: 16.3 MB (71.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,22 +1,28 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1Counter = [0 for _ in range(26)]
-        for char in s1:
-            s1Counter[ord(char)-97] += 1
-            
-        subCounter = [0 for _ in range(26)]
-        n = len(s1)
-        m =  len(s2)
-        left = 0
-        for right in range(m):
-            subCounter[ord(s2[right])-97] += 1
-            
-            while right-left+1 > n:
-                subCounter[ord(s2[left])-97] -= 1
-                left += 1
-
-            if right-left+1 == n and subCounter == s1Counter:
-                return True                
-            
+        n, m = len(s1), len(s2)
         
+        if m < n:
+            return False
+        
+        count = Counter(s1)
+        s1_count = [count[char] for char in ascii_lowercase]
+
+        def toIndex(char):
+            return ord(char) - ord('a')
+        
+        running_count = [0 for _ in range(len(ascii_lowercase))]
+
+        for i in range(n):
+            running_count[toIndex(s2[i])] += 1
+
+        if running_count == s1_count:
+            return True
+
+        for j in range(m-n):
+            running_count[toIndex(s2[j])] -= 1
+            running_count[toIndex(s2[j + n])] += 1
+            if running_count == s1_count:
+                return True
+
         return False
