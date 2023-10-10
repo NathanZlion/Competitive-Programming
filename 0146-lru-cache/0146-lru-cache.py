60b512b993dd7d8724c17cb72d3439c7a1dd431c
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-10 17:13:47 +0300

    Time: 705 ms (46.04%), Space: 78.6 MB (21.26%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,83 +1,70 @@
+
 class Node:
-    """ Node class with attributes key, value, prev and next. """
-    def __init__(self, key=-1, value=-1, prev=None, nxt=None):
+    def __init__(self, key, value, prev = None, next_ = None):
         self.key = key
         self.value = value
         self.prev = prev
-        self.next = nxt
+        self.next = next_
 
 
 class DoublyLinkedList:
-    """recent ones are at the tail, and least recent ones at the head."""
     def __init__(self):
-        # Added 2 dummy Nodes at head and tail as a 
-        # padding to simplify other implementations
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.dummyHead = Node(-1, -1)
+        self.dummyTail = Node(-1, -1)
+        self.dummyHead.next = self.dummyTail
+        self.dummyTail.prev = self.dummyHead
+        self._length = 0
 
-    def push(self, node: Node):
-        """push to the tail."""
-        last_node = self.tail.prev
-        last_node.next = node
-        node.prev = last_node
-        node.next = self.tail
-        self.tail.prev = node
+    def popLastNode(self) -> Node:
+        return self.popNode(self.dummyTail.prev)
 
-    def delete_node(self, node: Node) -> None:
-        prevv = node.prev
-        nextt = node.next
-        prevv.next = nextt
-        nextt.prev = prevv
+    def popNode(self, node: Node) -> Node:
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+        self._length -= 1
+        return node
 
-    def make_recent(self, node: Node):
-        """ brings a least recent node to the front """
-        self.delete_node(node)
-        self.push(node)
+    def appendNode(self, newNode: Node) -> None:
+        next_ = self.dummyHead.next
+        self.dummyHead.next = newNode
+        newNode.prev = self.dummyHead
+        newNode.next = next_
+        next_.prev = newNode
+        self._length += 1
 
-    def evict_least_recent(self) -> Node:
-        """ delete the node next to the dummy head """
-        node_to_evict = self.head.next
-        self.delete_node(node_to_evict)
-
-        return node_to_evict
-    
-    def get_least_recent( self ) -> Node:
-        return self.head.next
+    def __len__(self):
+        return self._length
 
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.dll = DoublyLinkedList()
         self.capacity = capacity
-        self.key_to_node = {}
+        self.map = {}
+        self.doublyLinkedList = DoublyLinkedList()
 
     def get(self, key: int) -> int:
-        if key in self.key_to_node:
-            node = self.key_to_node[key]
-            self.dll.make_recent(node)
-
+        if key in self.map:
+            node = self.map[key]
+            self.doublyLinkedList.popNode(node)
+            self.doublyLinkedList.appendNode(node)
             return node.value
 
         return -1
 
     def put(self, key: int, value: int) -> None:
-        # check if the node is in keys
-        # if in keys delete that and also the node
-        if key in self.key_to_node:
-            node = self.key_to_node[key]
-            del self.key_to_node[key]
-            self.dll.delete_node(node)
+        if key in self.map:
+            node = self.map[key]
+            self.doublyLinkedList.popNode(node)
+            self.doublyLinkedList.appendNode(node)
+            node.value = value
+        else:
+            newNode = Node(key, value)
+            self.map[key] = newNode
+            self.doublyLinkedList.appendNode(newNode)
 
-        # add it to hash and dll
-        new_node = Node(key, value)
-        self.key_to_node[key] = new_node
-        self.dll.push(new_node)
-
-        # remove any excess node
-        if len(self.key_to_node) > self.capacity:
-            node_to_evict = self.dll.get_least_recent()
-            self.dll.evict_least_recent()
-            del self.key_to_node[node_to_evict.key]
-
+        if len(self.doublyLinkedList) > self.capacity:
+            poppedNode = self.doublyLinkedList.popLastNode()
+            del self.map[poppedNode.key]
+            del poppedNode
