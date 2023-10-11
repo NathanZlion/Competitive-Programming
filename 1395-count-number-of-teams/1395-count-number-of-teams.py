90e1dc2c54c2f7ad4119f1ab1131167864f700f8
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-11 14:55:10 +0300

    Time: 2373 ms (5.21%), Space: 21.6 MB (5.43%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -4,32 +4,23 @@ class Solution:
         n = len(rating)
 
         @cache
-        def numOfTeamsForIncreasing(reqTeamSize: int, maxRating: int, start: int) -> int:
+        def subsequentTeams(reqTeamSize: int, maxRating: int, start: int, increasing: bool) -> int:
             if reqTeamSize == 0:
                 return 1
             
             res = 0
             for index in range(start, n):
-                if rating[index] > maxRating:
-                    res += numOfTeamsForIncreasing(reqTeamSize - 1, rating[index], index+1)
+                if rating[index] > maxRating and increasing:
+                    res += subsequentTeams(reqTeamSize - 1, rating[index], index+1, increasing)
+                
+                elif rating[index] < maxRating and not increasing:
+                    res += subsequentTeams(reqTeamSize - 1, rating[index], index+1, increasing)
             
-            return res
-
-        @cache
-        def numOfTeamsForDecreasing(reqTeamSize: int, minRating: int, start: int) -> int:
-            if reqTeamSize == 0:
-                return 1
-            
-            res = 0
-            for index in range(start, n):
-                if rating[index] < minRating:
-                    res += numOfTeamsForDecreasing(reqTeamSize - 1, rating[index], index+1)
-
             return res
 
         res = 0
         for index in range(n-1, -1, -1):
-            res += numOfTeamsForIncreasing(2, rating[index], index + 1)
-            res += numOfTeamsForDecreasing(2, rating[index], index + 1)
+            res += subsequentTeams(2, rating[index], index + 1, True)
+            res += subsequentTeams(2, rating[index], index + 1, False)
 
         return res
