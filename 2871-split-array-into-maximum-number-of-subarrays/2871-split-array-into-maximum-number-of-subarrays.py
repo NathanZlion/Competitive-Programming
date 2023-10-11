1ab2d05ff2178566cc6a3d20c388aff494c841e5
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-11 17:45:49 +0300

    Time: 875 ms (26.19%), Space: 28.1 MB (6.80%) - LeetHub

---
+++ 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxSubarrays(self, nums: List[int]) -> int:
+
+        runningAnd = 2 ** 32 - 1
+        for num in nums:
+            runningAnd &= num
+
+        if runningAnd != 0:
+            return 1
+
+        count = 0
+        runningAnd = 2 ** 32 - 1
+        for index in range(len(nums)):
+            num = nums[index]
+            runningAnd &= num
+            if runningAnd == 0:
+                count += 1
+
+                if index < len(nums)-1:
+                    runningAnd = nums[index+1]  
+                else:
+                    break
+        return count
