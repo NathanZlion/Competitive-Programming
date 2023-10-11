68117ba23b6b90a84ee8fd5c972da53d7ac41a27
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-11 15:07:38 +0300

    Time: 630 ms (68.33%), Space: 16.5 MB (57.24%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -1,26 +1,17 @@
 class Solution:
-    def numTeams(self, rating: List[int]) -> int:
+    def numTeams(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        upper_dps = [0] * n
+        lower_dps = [0] * n
+
         count = 0
-        n = len(rating)
+        for i in range(len(ratings)):
+            for j in range(i):
+                if ratings[j] < ratings[i]:
+                    count += upper_dps[j]
+                    upper_dps[i] += 1
+                else:
+                    count += lower_dps[j]
+                    lower_dps[i] += 1
 
-        @cache
-        def subsequentTeams(reqTeamSize: int, maxRating: int, start: int, increasing: bool) -> int:
-            if reqTeamSize == 0:
-                return 1
-            
-            res = 0
-            for index in range(start, n):
-                if rating[index] > maxRating and increasing:
-                    res += subsequentTeams(reqTeamSize - 1, rating[index], index+1, increasing)
-                
-                elif rating[index] < maxRating and not increasing:
-                    res += subsequentTeams(reqTeamSize - 1, rating[index], index+1, increasing)
-            
-            return res
-
-        res = 0
-        for index in range(n-1, -1, -1):
-            res += subsequentTeams(2, rating[index], index + 1, True)
-            res += subsequentTeams(2, rating[index], index + 1, False)
-
-        return res
+        return count
