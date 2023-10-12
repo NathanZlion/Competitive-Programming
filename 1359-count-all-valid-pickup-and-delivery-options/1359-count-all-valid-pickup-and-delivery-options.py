bd394435f4d6f7ad25c0a17c895d01b182b087b3
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-12 19:00:02 +0300

    Time: 37 ms (70.84%), Space: 16.3 MB (37.03%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -2,4 +2,10 @@ class Solution:
     def countOrders(self, n: int) -> int:
         MOD = (10**9)+7
 
-        return math.factorial(2 * n) // pow(2, n) % MOD
+        possibilities = 1
+        for i in range(1, n+1):
+            positions = (2*i) - 1
+            possibilities *= (positions*(1+positions))//2
+            possibilities %= MOD
+
+        return possibilities
