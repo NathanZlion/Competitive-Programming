4695fd68df373d72e13f26d37edb66b349a8e463
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-12 12:50:43 +0300

    Time: 47 ms (5.89%), Space: 17.2 MB (25.43%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -10,7 +10,6 @@ class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
         N = mountain_arr.length()
 
-        # binary search
         def find_peak_of_mount_arr(mount_arr: 'MountainArray') -> int:
             nonlocal N
             left, right = -1, N
@@ -50,4 +49,11 @@ class Solution:
         peakIdx = find_peak_of_mount_arr(mountain_arr)
         left_half_res = find_target(mountain_arr, -1, peakIdx + 1, True, target)
 
-        return left_half_res if left_half_res != -1 else find_target(mountain_arr, peakIdx-1, N, False, target)
+        if left_half_res != -1:
+            return left_half_res
+        else:
+            return find_target(mountain_arr, peakIdx-1, N, False, target)
+   
+
+
+
