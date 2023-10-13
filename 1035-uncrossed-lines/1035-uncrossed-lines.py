f969e5ec494166d532823e352f50704ab079b72a
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-13 18:56:53 +0300

    Time: 142 ms (72.05%), Space: 16.4 MB (86.28%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -6,18 +6,13 @@ class Solution:
         dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
         for r in range(m-1, -1, -1):
             for c in range(n-1, -1, -1):
-                if nums1[c] == nums2[r]:
-                    dp[r][c] = max (
-                        1 + dp[r+1][c+1],
-                        dp[r+1][c],
-                        dp[r][c+1],
-                    )
+                dp[r][c] = max (
+                    dp[r+1][c],
+                    dp[r][c+1],
+                )
 
-                else:
-                    dp[r][c] = max (
-                        dp[r+1][c+1],
-                        dp[r+1][c],
-                        dp[r][c+1],
-                    )
+                if nums1[c] == nums2[r]:
+                    dp[r][c] = max(dp[r][c], 1 + dp[r+1][c+1])
+
 
         return dp[0][0]
