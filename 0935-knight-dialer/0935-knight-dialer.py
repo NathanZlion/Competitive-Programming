2aa11dc0b3b865fa3dba536a952faa6be0e982e2
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-13 08:00:02 +0300

    Time: 8091 ms (5.06%), Space: 75 MB (18.43%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -4,11 +4,12 @@ class Solution:
         non_numeric_cells = [(3, 0), (3, 2)]
         PHONE_PAD_ROWS = 4
         PHONE_PAD_COLS = 3
+        modulus = (10**9 + 7)
         sys.setrecursionlimit(10005)
-        
+
         def is_inbound(row, col):
             return 0 <= row < PHONE_PAD_ROWS and 0 <= col < PHONE_PAD_COLS
-        
+
         @cache
         def possibilities(row, col, jumps_left):
             if (row, col) in non_numeric_cells or not is_inbound(row, col):
@@ -20,12 +21,12 @@ class Solution:
             res = 0
             for r, c in knight_jumps:
                 res += possibilities(row + r, col + c, jumps_left -1)
-            
-            return res % (10**9 + 7)
+
+            return res % modulus
 
         res = 0
         for row in range(4):
             for col in range(3):
                 res += possibilities(row, col, n-1)
 
-        return res % (10**9 + 7)
+        return res % modulus
