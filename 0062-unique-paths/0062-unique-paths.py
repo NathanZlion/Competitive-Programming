06b484266fa6cd447f40e9a1affb2df1a86ebe75
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-15 20:05:44 +0300

    Time: 26 ms (99.03%), Space: 16.3 MB (67.08%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        grid = [[0]*n for _ in range(m)]
-        grid[0][0] = 1
-        
-        def is_inbound(row: int, col: int) -> bool:
-            return 0 <= col < n and 0 <= row < m
-        
-        for row in range(m):
-            for col in range(n):
-                if is_inbound(row-1, col):
-                    grid[row][col] +=  grid[row-1][col]
-                
-                if is_inbound(row, col-1):
-                    grid[row][col] +=  grid[row][col-1]
-
-        return grid[m-1][n-1]
+        return int(factorial(m + n - 2) / (factorial(n - 1) * factorial(m - 1)))
