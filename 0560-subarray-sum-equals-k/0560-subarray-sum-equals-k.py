60fe5385f799e2bf55649740e0508c0b614344c8
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-16 16:18:19 +0300

    Time: 263 ms (39.68%), Space: 18.9 MB (41.96%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -4,13 +4,13 @@ class Solution:
         
         count = 0
         
-        prefixMap = defaultdict(int)
+        prefixMap = {}
         prefixMap[0] = 1
         runningSum = 0
 
         for index in range(n):
             runningSum += nums[index]
-            count += prefixMap[runningSum - k]
-            prefixMap[runningSum] += 1
+            count += prefixMap.get(runningSum - k, 0)
+            prefixMap[runningSum] = prefixMap.get(runningSum, 0) + 1
 
         return count
