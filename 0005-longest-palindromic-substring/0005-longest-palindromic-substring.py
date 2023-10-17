1d32e1c412393a5f46f4a3b8a89a9e5c82ec09eb
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-17 11:44:38 +0300

    Time: 3419 ms (25.03%), Space: 25.1 MB (6.60%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -12,15 +12,19 @@ class Solution:
         for i in range(n - 1):
             if s[i] == s[i + 1]:
                 dp[i][i + 1] = True
+                # guaranteed this has at least equal length with the prev and
+                # cos we're iterating length in increasing order
                 ans = [i, i + 1]
 
+        # iterate throught different letter sizes
         for length in range(3, n+1):
             for left in range(n - length + 1):
                 right = left + length - 1
+
                 if s[left] == s[right] and dp[left + 1][right - 1]:
                     dp[left][right] = True
                     ans = [left, right]
 
-        left, right = ans
 
+        left, right = ans
         return s[left: right + 1]
