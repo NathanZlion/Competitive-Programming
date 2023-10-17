1278a9a1f9ddaf455a92a2bb28ee3cc7c4324222
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-17 16:16:58 +0300

    Time: 689 ms (66.84%), Space: 157.1 MB (10.45%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,22 +1,21 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        """
-        do a total sum of the array
-        if it is odd, then it's always false -> we can prove this
-        """
+
         self.total_sum = sum(nums)
+        
         if self.total_sum % 2: #this means it's odd
             return False
         
         self.half_sum = self.total_sum // 2
+
         @cache
         def dp(index: int, runningSum: int) -> bool:
             if runningSum == self.half_sum:
                 return True
             
-            if index >= len(nums):
+            if index >= len(nums) or runningSum > self.half_sum:
                 return False
-            
+
             return dp(index+1, runningSum + nums[index]) or dp(index+1, runningSum ) 
 
         return dp(0, 0)
