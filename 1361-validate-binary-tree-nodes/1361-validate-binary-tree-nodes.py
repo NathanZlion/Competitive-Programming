c2377bbd46184287a93814d6f06b67d0a3afb8b2
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-17 10:03:04 +0300

    Time: 263 ms (79.64%), Space: 18.5 MB (85.63%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -0,0 +1,42 @@
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        indegreeCount = defaultdict(int)
+        
+        for node, (left, right) in enumerate(zip(leftChild, rightChild)):
+            if left != -1:
+                indegreeCount[left] += 1
+
+            if right != -1:
+                indegreeCount[right] += 1
+
+        rootNode = -1
+        for node in range(n):
+            if indegreeCount[node] == 0:
+                rootNode = node
+                break
+
+        if rootNode == -1:
+            return False
+
+        visited = [False for _ in range(n)]
+        queue = deque()
+        queue.append(rootNode)
+        
+        while queue:
+            curr = queue.popleft()
+            if visited[curr]:
+                return False
+
+            visited[curr] = True
+            
+            if leftChild[curr] != -1:
+                queue.append(leftChild[curr])
+            
+            if rightChild[curr] != -1:
+                queue.append(rightChild[curr])
+
+        for isVisited in visited:
+            if not isVisited:
+                return False
+            
+        return True
