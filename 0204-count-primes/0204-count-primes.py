3793b0a90906615d5aca17ff11b58ac5e4d85bf0
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-18 09:59:55 +0300

    Time: 3508 ms (26.31%), Space: 69.8 MB (42.32%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,15 +1,14 @@
 class Solution:
-    
-    def primes_count(self, n: int) -> int:
+    def countPrimes(self, n: int) -> int:
         """using prime sieve counts the number of prime numbers upto n"""
         
         if n < 3:
             return 0
+
         is_prime = [True for _ in range(n)]
         is_prime[0] = is_prime[1] = False
 
         i = 2
-
         while i * i < n:
             if is_prime[i]:
                 j = 2 * i
@@ -26,7 +25,3 @@ class Solution:
                 count += 1
 
         return count
-
-    def countPrimes(self, n: int) -> int:
-
-        return self.primes_count(n)
