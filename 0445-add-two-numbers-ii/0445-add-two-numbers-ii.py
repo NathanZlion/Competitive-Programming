b35be07484f09269180ea5ca5a760347b22bd479
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-19 09:57:21 +0300

    Time: 64 ms (70.13%), Space: 16.4 MB (43.73%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -5,34 +5,42 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # if either one is an empty linked list return the other one
-        if not l1 or not l2:
-            return l2 if not l1 else l1
-
+        """
+        This shows that I have to put all nodes into a stack first then
+        pop them to go from th stack
+        """
         stack1 = []
+        stack2 = []
         while l1:
             stack1.append(l1.val)
             l1 = l1.next
 
-        stack2= []
         while l2:
             stack2.append(l2.val)
             l2 = l2.next
         
-        res = None
+        place = 0  # 0th, 10th place ...
         carry = 0
-
-        while stack1 or stack2 or carry:
-            num = carry
+        res = 0
+        while stack1 or stack2 or carry > 0:
+            curr_digit = 0
             if stack1:
-                num += stack1.pop()
-            
-            if stack2:
-                num += stack2.pop()
-            
-            newNode = ListNode(val = num%10, next = res)
-            newNode.next = res
-            res = newNode
-            carry = num//10
+                curr_digit += stack1.pop()
 
-        return res
+            if stack2:
+                curr_digit += stack2.pop()
+
+            curr_digit += carry
+            carry = curr_digit // 10
+            curr_digit %= 10
+            res += curr_digit * (10**place)
+            
+            place += 1
+
+        dummy_head = ListNode(-1)
+        cur = dummy_head
+        for c in str(res):
+            cur.next = ListNode(int(c))
+            cur = cur.next
+        
+        return dummy_head.next
