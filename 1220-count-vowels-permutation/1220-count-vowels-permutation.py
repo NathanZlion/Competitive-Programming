ca984730582d1e2db8bd7337cfbb605df6a8049f
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-29 12:54:39 +0300

    Time: 448 ms (42.06%), Space: 21.9 MB (46.38%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        modulus = 1_000_000_007
+        preceeded = {
+            0: [1, 2, 4],
+            1: [0, 2],
+            2: [1, 3],
+            3: [2],
+            4: [2, 3]
+        }
+
+        dp = [[0 for _ in range(5)] for _ in range(n+1)]
+        for i in range(5):
+            dp[1][i] = 1
+
+        for length in range(2, n+1):
+            for vowel in range(5):
+                for preceeder in preceeded[vowel]:
+                    dp[length][vowel] += dp[length - 1][preceeder]
+                    dp[length][vowel] %= modulus
+        
+        return sum(dp[n]) % modulus
