bba3ea2cc18133fe6584d2bad7b2802e25593709
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-30 19:49:16 +0300

    Time: 1160 ms (51.95%), Space: 41.6 MB (60.61%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,18 +1,11 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-
-        alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         def wrap(index):
             index -= 97
-            # while index < 0:
-            #     index += 26
-            # if index < 0:
-            #     offset = 26 * ((index//26)+2)
-            #     index += offset
             if index > 25 or index <0:
                 index = index % 26
 
-            return alphabet[index]
+            return ascii_lowercase[index]
 
 
         sh = [0 for _ in range(len(s)+1)]
@@ -33,6 +26,5 @@ class Solution:
         for i in range(n - 1):
             runningSum += sh[i]
             res.append(wrap(ord(s[i]) + runningSum))
-        
-        return "".join(res)
 
+        return "".join(res)
