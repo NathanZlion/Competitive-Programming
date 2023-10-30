caf6a91e657f0d40169517443f4bd801d93ac4c3
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-10-30 17:29:03 +0300

    Time: 76 ms (15.45%), Space: 16.8 MB (87.20%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        res = []
+
+        def traverse(node: TreeNode | None, parent_deleted: bool) -> None:
+            if not node:
+                return
+        
+            to_be_deleted = node.val in to_delete
+            if parent_deleted and not to_be_deleted:
+                res.append(node)
+                
+            traverse(node.left, to_be_deleted)
+            traverse(node.right, to_be_deleted)
+
+            if node.left and node.left.val in to_delete:
+                node.left = None
+            
+            if node.right and node.right.val in to_delete:
+                node.right = None
+
+        traverse(root, True)
+        return res
