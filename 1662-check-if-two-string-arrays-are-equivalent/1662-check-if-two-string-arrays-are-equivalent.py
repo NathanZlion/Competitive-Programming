9be18ae67dd77a8b512e33a2132efeb63afbd1a1
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2023-12-01 21:33:16 +0300

    Time: 52 ms (5.43%), Space: 16.4 MB (28.02%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -1,3 +1,34 @@
 class Solution:
+    def total_length(self, arr):
+        res= 0
+        for word in arr:
+            res += len(word)
+        return res
+
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        return "".join(word1) == "".join(word2)
+        word1_char_length = self.total_length(word1)
+        word2_char_length = self.total_length(word2)
+        
+        if word1_char_length != word2_char_length:
+            return False
+        
+        word1_word_ptr = 0
+        word1_char_ptr = 0
+        word2_word_ptr = 0
+        word2_char_ptr = 0
+        for _ in range(word1_char_length):
+            if word1_char_ptr >= len(word1[word1_word_ptr]):
+                word1_char_ptr = 0
+                word1_word_ptr += 1
+            
+            if word2_char_ptr >= len(word2[word2_word_ptr]):
+                word2_char_ptr = 0
+                word2_word_ptr += 1
+            
+            if word1[word1_word_ptr][word1_char_ptr] != word2[word2_word_ptr][word2_char_ptr]:
+                return False
+            
+            word1_char_ptr += 1
+            word2_char_ptr += 1
+
+        return True
