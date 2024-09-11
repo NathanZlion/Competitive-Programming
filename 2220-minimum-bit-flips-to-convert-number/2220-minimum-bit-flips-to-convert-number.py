1298f399742cd9eb93041651d571db6243a43953
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2024-09-11 11:31:40 +0300

    Time: 25 ms (97.25%), Space: 16.4 MB (69.15%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,14 +1,3 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        """
-        Given X, binary format
-        
-        7, 10
-        
-        00000111
-        00001010
-        
-        3 flips
-        """
-
         return (start ^ goal).bit_count()
