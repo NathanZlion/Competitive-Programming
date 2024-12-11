af68e60e0bb6eadcdb2cf701b33c371a11443c2d
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2024-12-11 14:11:29 +0300

    Time: 1671 ms (21.92%), Space: 30 MB (84.93%) - LeetHub

--- 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
+++ 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
@@ -8,16 +8,15 @@ class Solution:
             # let's try to exclude the current index
             numToExclude = nums[index]
             targetSum = totalSum - numToExclude
-            
-            if targetSum % 2:
+
+            if targetSum % 2 > 0:
                 continue
-            
+
             count[numToExclude] -= 1
-            targetNum = targetSum // 2
-            
+
             # is the rest of the list sum to one of the nums
             # in the list
-            if count[targetNum] > 0:
+            if count[targetSum // 2] > 0:
                 maxOutlier = max(maxOutlier, numToExclude)
 
             count[numToExclude] += 1
