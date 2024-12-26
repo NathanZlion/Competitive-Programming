32f918393c8662d0b8e73f7f72826174d3e12d0c
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2024-12-26 23:02:00 +0300

    Time: 3 ms (6.74%), Space: 17.6 MB (8.95%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,41 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        modified = False
+        length = len(nums)
+        maxNumToRight = -1
+        
+        # try finding the next bigger permutation
+        for index in range(length - 1, -1, -1):
+            if nums[index] >= maxNumToRight:
+                maxNumToRight = nums[index]
+                continue
+            
+            modified = True
+            # find the immediate smallest num to the right
+            # with binary search
+            left = index
+            right = length
+            
+            while right > left + 1:
+                mid = (right + left) // 2
+                if nums[mid] > nums[index]:
+                    left = mid
+                else:
+                    right = mid
+
+            nums[left], nums[index] = nums[index], nums[left]
+            
+            # make sure the side from index + 1 to end is minimum            
+            for offset, num in enumerate(sorted(nums[index+1:])):
+                nums[offset + index + 1] = num
+            
+            modified = True
+            break
+            
+        # means the arrary is sorted in reverse order
+        if not modified:
+            for index in range(length // 2):
+                nums[index], nums[length - index - 1] = nums[length - index - 1], nums[index]
